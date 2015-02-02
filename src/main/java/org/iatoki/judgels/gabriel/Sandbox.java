bedaec848a04a8ec65ef45df31e324d773988c25
Ashar Fuadi <fushar@gmail.com>
2015-02-02 15:15:44 +0700

    Add evaluator for interactive problems

--- src/main/java/org/iatoki/judgels/gabriel/Sandbox.java
+++ src/main/java/org/iatoki/judgels/gabriel/Sandbox.java
@@ -1,42 +1,51 @@
 package org.iatoki.judgels.gabriel;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
-public interface Sandbox {
+public abstract class Sandbox {
 
-    void addFile(File file);
+    public abstract void addFile(File file);
 
-    boolean containsFile(String filename);
+    public abstract boolean containsFile(String filename);
 
-    File getFile(String filename);
+    public abstract File getFile(String filename);
 
-    void addAllowedDirectory(File directory);
+    public abstract void addAllowedDirectory(File directory);
 
-    void setTimeLimitInMilliseconds(int timeLimit);
+    public abstract void setTimeLimitInMilliseconds(int timeLimit);
 
-    void setMemoryLimitInKilobytes(int memoryLimit);
+    public abstract void setMemoryLimitInKilobytes(int memoryLimit);
 
-    void setStackSizeInKilobytes(int stackSizeInKilobytes);
+    public abstract void setStackSizeInKilobytes(int stackSizeInKilobytes);
 
-    void setMaxProcesses(int maxProcesses);
+    public abstract void setMaxProcesses(int maxProcesses);
 
-    void setStandardInput(String filenameInsideThisSandbox);
+    public abstract void setStandardInput(String filenameInsideThisSandbox);
 
-    void setStandardOutput(String filenameInsideThisSandbox);
+    public abstract void setStandardOutput(String filenameInsideThisSandbox);
 
-    void setStandardError(String filenameInsideThisSandbox);
+    public abstract void setStandardError(String filenameInsideThisSandbox);
 
-    void setStandardInput(File file);
+    public abstract void removeAllFilesExcept(Set<String> filenamesToRetain);
 
-    void setStandardOutput(File file);
+    public abstract void cleanUp();
 
-    void setStandardError(File file);
+    public final SandboxExecutionResult execute(List<String> command) {
+        ProcessBuilder pb = getProcessBuilder(command);
+        int exitCode;
+        try {
+            exitCode = pb.start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            return new SandboxExecutionResult(SandboxExecutionStatus.INTERNAL_ERROR, new SandboxExecutionResultDetails(1, 100, 1000, "Execution error"));
+        }
 
-    void removeAllFilesExcept(Set<String> filenamesToRetain);
+        return getResult(exitCode);
+    }
 
-    void cleanUp();
+    protected abstract ProcessBuilder getProcessBuilder(List<String> command);
 
-    SandboxExecutionResult execute(List<String> command);
+    protected abstract SandboxExecutionResult getResult(int exitCode);
 }
