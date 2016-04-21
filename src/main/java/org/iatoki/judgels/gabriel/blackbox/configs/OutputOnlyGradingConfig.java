b032d744df4da79819e43dabf5fe45da3ae8c906
Ashar Fuadi <fushar@gmail.com>
2016-04-22 00:45:05 +0100

    Change display name of source field for output-only engine

--- src/main/java/org/iatoki/judgels/gabriel/blackbox/configs/OutputOnlyGradingConfig.java
+++ src/main/java/org/iatoki/judgels/gabriel/blackbox/configs/OutputOnlyGradingConfig.java
@@ -1,12 +1,19 @@
 package org.iatoki.judgels.gabriel.blackbox.configs;
 
+import com.google.common.collect.ImmutableMap;
 import org.iatoki.judgels.gabriel.blackbox.TestGroup;
 
 import java.util.List;
+import java.util.Map;
 
 public final class OutputOnlyGradingConfig extends SingleSourceFileWithoutSubtasksBlackBoxGradingConfig {
 
     public OutputOnlyGradingConfig(List<TestGroup> testData) {
         super(0, 0, testData);
+    }
+
+    @Override
+    public Map<String, String> getSourceFileFields() {
+        return ImmutableMap.of("source", "Output files (.zip)");
     }
 }
