18716433c57606bb96b6d44ea147667aaacf28cd
Ashar Fuadi <fushar@gmail.com>
2017-08-30 22:01:00 +0700

    Initial repo setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# sealtiel
+
+[![Build Status](https://travis-ci.org/judgels-ng/sealtiel.svg?branch=master)](https://travis-ci.org/judgels-ng/sealtiel)
---
+++ sealtiel-server/src/main/java/judgels/sealtiel/SealtielServer.java
@@ -0,0 +1,9 @@
+package judgels.sealtiel;
+
+public class SealtielServer {
+    private SealtielServer() {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
