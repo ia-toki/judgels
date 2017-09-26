73185d19d259174e2c44b05a544af4407bf99ca2
Ashar Fuadi <fushar@gmail.com>
2017-09-26 12:17:01 +0700

    Correctly add @JsonDeserialize to Message

--- sealtiel-api/src/main/java/judgels/sealtiel/api/message/Message.java
+++ sealtiel-api/src/main/java/judgels/sealtiel/api/message/Message.java
@@ -1,8 +1,10 @@
 package judgels.sealtiel.api.message;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@JsonDeserialize(as = ImmutableMessage.class)
 public interface Message {
     long getId();
     String getSourceJid();
