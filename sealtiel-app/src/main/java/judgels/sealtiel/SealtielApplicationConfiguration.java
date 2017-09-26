217252a291e76cc001d14abdd9f9d45e92e1de75
Ashar Fuadi <fushar@gmail.com>
2017-09-26 14:09:08 +0700

    Actually make Dropwizard config works

--- sealtiel-app/src/main/java/judgels/sealtiel/SealtielApplicationConfiguration.java
+++ sealtiel-app/src/main/java/judgels/sealtiel/SealtielApplicationConfiguration.java
@@ -1,16 +1,18 @@
 package judgels.sealtiel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.dropwizard.Configuration;
-import org.immutables.value.Value;
 
-@Value.Immutable
-@JsonDeserialize(as = ImmutableSealtielApplicationConfiguration.class)
-public abstract class SealtielApplicationConfiguration extends Configuration {
-    @JsonProperty("sealtiel")
-    public abstract SealtielConfiguration getSealtielConfig();
+public class SealtielApplicationConfiguration extends Configuration {
+    private final SealtielConfiguration sealtielConfig;
 
-    @Override
-    public abstract String toString();
+    public SealtielApplicationConfiguration(
+            @JsonProperty("sealtiel") SealtielConfiguration sealtielConfig) {
+
+        this.sealtielConfig = sealtielConfig;
+    }
+
+    public SealtielConfiguration getSealtielConfig() {
+        return sealtielConfig;
+    }
 }
