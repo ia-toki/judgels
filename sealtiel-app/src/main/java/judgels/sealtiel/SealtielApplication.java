5533cc0eda109eebdae62a07c0b9f78a1b96829a
Ashar Fuadi <fushar@gmail.com>
2017-09-26 12:11:06 +0700

    Fix http-remoting's empty optional issue

--- sealtiel-app/src/main/java/judgels/sealtiel/SealtielApplication.java
+++ sealtiel-app/src/main/java/judgels/sealtiel/SealtielApplication.java
@@ -2,7 +2,11 @@ package judgels.sealtiel;
 
 import com.palantir.remoting3.servers.jersey.HttpRemotingJerseyFeature;
 import io.dropwizard.Application;
+import io.dropwizard.jersey.optional.EmptyOptionalException;
 import io.dropwizard.setup.Environment;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 import judgels.sealtiel.rabbitmq.RabbitMQModule;
 
 public class SealtielApplication extends Application<SealtielApplicationConfiguration> {
@@ -13,6 +17,7 @@ public class SealtielApplication extends Application<SealtielApplicationConfigur
     @Override
     public void run(SealtielApplicationConfiguration config, Environment env) throws Exception {
         env.jersey().register(HttpRemotingJerseyFeature.INSTANCE);
+        env.jersey().register(new EmptyOptionalExceptionMapper());
 
         SealtielConfiguration sealtielConfig = config.getSealtielConfig();
         SealtielComponent component = DaggerSealtielComponent.builder()
@@ -21,5 +26,14 @@ public class SealtielApplication extends Application<SealtielApplicationConfigur
                 .build();
 
         env.jersey().register(component.messageResource());
+    }
+
+    // https://github.com/palantir/http-remoting/issues/427
+    @Provider
+    private static class EmptyOptionalExceptionMapper implements ExceptionMapper<EmptyOptionalException> {
+        @Override
+        public Response toResponse(EmptyOptionalException exception) {
+            return Response.noContent().build();
+        }
     }
 }
