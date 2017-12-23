bca19d6de17adda5f94bd3d8571f3bafa41aef4d
Ashar Fuadi <fushar@gmail.com>
2017-12-23 20:19:41 +0700

    Remove jid prefix in UserRegistrationEmailModel

--- jophiel-app/src/main/java/judgels/jophiel/user/email/UserRegistrationEmailModel.java
+++ jophiel-app/src/main/java/judgels/jophiel/user/email/UserRegistrationEmailModel.java
@@ -2,12 +2,10 @@ package judgels.jophiel.user.email;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import judgels.persistence.JidPrefix;
 import judgels.persistence.Model;
 
 @SuppressWarnings("checkstyle:visibilitymodifier")
 @Entity(name = "jophiel_user_registration_email")
-@JidPrefix("USEE")
 public class UserRegistrationEmailModel extends Model {
     @Column(unique = true, nullable = false)
     public String userJid;
