06070951135739fb53a2a016df6cad3f0afb7671
Ashar Fuadi <fushar@gmail.com>
2018-01-02 23:37:54 +0700

    Install JudgelsJerseyFeature

--- sealtiel-app/src/main/java/judgels/sealtiel/SealtielApplication.java
+++ sealtiel-app/src/main/java/judgels/sealtiel/SealtielApplication.java
@@ -1,13 +1,9 @@
 package judgels.sealtiel;
 
-import com.palantir.remoting3.servers.jersey.HttpRemotingJerseyFeature;
 import io.dropwizard.Application;
-import io.dropwizard.jersey.optional.EmptyOptionalException;
 import io.dropwizard.setup.Environment;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
 import judgels.sealtiel.rabbitmq.RabbitMQModule;
+import judgels.service.jersey.JudgelsJerseyFeature;
 
 public class SealtielApplication extends Application<SealtielApplicationConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -16,27 +12,17 @@ public class SealtielApplication extends Application<SealtielApplicationConfigur
 
     @Override
     public void run(SealtielApplicationConfiguration config, Environment env) throws Exception {
-        env.jersey().register(HttpRemotingJerseyFeature.INSTANCE);
-        env.jersey().register(new EmptyOptionalExceptionMapper());
-
         SealtielConfiguration sealtielConfig = config.getSealtielConfig();
         SealtielComponent component = DaggerSealtielComponent.builder()
                 .sealtielModule(new SealtielModule(sealtielConfig))
                 .rabbitMQModule(new RabbitMQModule(sealtielConfig.getRabbitMQConfig()))
                 .build();
 
+        env.jersey().register(JudgelsJerseyFeature.INSTANCE);
+
         env.jersey().register(component.versionResource());
         env.jersey().register(component.messageResource());
 
         env.healthChecks().register("rabbitmq", component.rabbitmqHealthCheck());
-    }
-
-    // https://github.com/palantir/http-remoting/issues/427
-    @Provider
-    private static class EmptyOptionalExceptionMapper implements ExceptionMapper<EmptyOptionalException> {
-        @Override
-        public Response toResponse(EmptyOptionalException exception) {
-            return Response.noContent().build();
-        }
     }
 }
