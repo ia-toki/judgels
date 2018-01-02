66c23050973bc3a0f771ce6b158da255bc8d660d
Ashar Fuadi <fushar@gmail.com>
2018-01-02 23:39:40 +0700

    Add deser test for sealtiel.yml

---
+++ sealtiel-app/src/test/java/judgels/sealtiel/SealtielApplicationConfigurationTests.java
@@ -0,0 +1,26 @@
+package judgels.sealtiel;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.configuration.YamlConfigurationFactory;
+import io.dropwizard.jackson.Jackson;
+import io.dropwizard.jersey.validation.Validators;
+import java.io.File;
+import java.nio.file.Paths;
+import javax.validation.Validator;
+import org.junit.jupiter.api.Test;
+
+class SealtielApplicationConfigurationTests {
+    private final ObjectMapper objectMapper = Jackson.newObjectMapper();
+    private final Validator validator = Validators.newValidator();
+    private final YamlConfigurationFactory<SealtielApplicationConfiguration> factory =
+            new YamlConfigurationFactory<>(SealtielApplicationConfiguration.class, validator, objectMapper, "dw");
+
+    @Test
+    void can_deserialize_sealtiel_yml() {
+        File jophielYml = Paths.get("..", "sealtiel-dist", "var", "conf", "sealtiel.yml").toFile();
+        assertThatCode(() -> factory.build(jophielYml))
+                .doesNotThrowAnyException();
+    }
+}
