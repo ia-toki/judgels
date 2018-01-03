5c5822cbec590165e8a0c24bcdb6c1138b1d70f0
Ashar Fuadi <fushar@gmail.com>
2018-01-03 14:17:02 +0700

    Add integration tests

--- sealtiel-app/src/main/java/judgels/sealtiel/SealtielConfiguration.java
+++ sealtiel-app/src/main/java/judgels/sealtiel/SealtielConfiguration.java
@@ -14,4 +14,6 @@ public interface SealtielConfiguration {
 
     @JsonProperty("rabbitmq")
     RabbitMQConfiguration getRabbitMQConfig();
+
+    class Builder extends ImmutableSealtielConfiguration.Builder {}
 }
