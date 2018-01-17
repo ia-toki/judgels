2f38503d809b656ef43a7706eee3412401dec6d8
Ashar Fuadi <fushar@gmail.com>
2018-01-17 15:14:31 +0700

    Migrate judgels-recaptcha

---
+++ judgels-recaptcha/src/main/java/judgels/recaptcha/RecaptchaConfiguration.java
@@ -0,0 +1,13 @@
+package judgels.recaptcha;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonDeserialize(as = ImmutableRecaptchaConfiguration.class)
+public interface RecaptchaConfiguration {
+    String getSiteKey();
+    String getSecretKey();
+
+    class Builder extends ImmutableRecaptchaConfiguration.Builder {}
+}
---
+++ judgels-recaptcha/src/main/java/judgels/recaptcha/RecaptchaWebConfiguration.java
@@ -0,0 +1,18 @@
+package judgels.recaptcha;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonDeserialize(as = ImmutableRecaptchaWebConfiguration.class)
+public interface RecaptchaWebConfiguration {
+    String getSiteKey();
+
+    static RecaptchaWebConfiguration fromServerConfig(RecaptchaConfiguration config) {
+        return new RecaptchaWebConfiguration.Builder()
+                .siteKey(config.getSiteKey())
+                .build();
+    }
+
+    class Builder extends ImmutableRecaptchaWebConfiguration.Builder {}
+}
