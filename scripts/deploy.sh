12e512fd415646485acd96bf36aa2a57fd3bfe16
Jordan Fernando <fernandojordan.92@gmail.com>
2018-02-04 02:11:12 +0700

    Fix mistake var in deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ ansible --version
 
 ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel.yml
 ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel-nginx.yml
-ansible-playbook -e db_user=$db_user -e db_password=$db_password playbooks/deploy-jophiel.yml
+ansible-playbook -e db_username=$db_username -e db_password=$db_password playbooks/deploy-jophiel.yml
