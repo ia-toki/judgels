00ffe72a02f00b7b17a8e7ce2ada351a8c4552ac
Jordan Fernando <fernandojordan.92@gmail.com>
2018-02-04 01:48:16 +0700

    Add ansible version.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname "$0")"/../jophiel-dist/ansible
 
+ansible --version
+
 ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel.yml
 ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel-nginx.yml
 ansible-playbook -e db_user=$db_user -e db_password=$db_password playbooks/deploy-jophiel.yml
