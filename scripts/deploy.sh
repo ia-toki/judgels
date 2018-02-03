a8e7cf266563371c98b81b4c35df8df8c38e7327
Jordan Fernando <fernandojordan.92@gmail.com>
2018-02-04 00:48:39 +0700

    Update travis to install latest ansible version.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"/../jophiel-dist/ansible
-ansible --version
 
 ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel.yml
 ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel-nginx.yml
