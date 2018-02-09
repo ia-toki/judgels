0c383040ac1559882bb9de83d59022c31f7a7cfe
Ashar Fuadi <fushar@gmail.com>
2018-02-09 15:05:50 +0700

    raphael.js.example

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,7 +1,8 @@
 set +ex
 
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)/.."
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 TAG=$1
 
 GENERATE_SOURCEMAP=false yarn build
+mv $DIR/build/var/conf/raphael.js.example $DIR/build/var/conf/raphael.js
 docker build -t judgels/raphael:$TAG $DIR
