8b737171ca28094e50bf8e450976194f7f437113
Nathan Azaria <nathanajah@gmail.com>
2018-02-13 23:08:17 +0000

    Re-add missing sccs file (#46)

--- src/components/LoadingPage/LoadingPage.tsx
+++ src/components/LoadingPage/LoadingPage.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 
 import { SingleColumnLayout } from '../layouts/SingleColumnLayout/SingleColumnLayout';
 
+import './LoadingPage.css';
+
 export const LoadingPage = () => (
   <SingleColumnLayout>
     <Spinner className="loading-spinner" />
