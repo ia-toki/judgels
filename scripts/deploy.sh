5a21f023103f2dfc947f18b2e9f111692fb6ba2d
Jordan Fernando <fernandojordan.92@gmail.com>
2018-02-19 22:25:38 +0800

    Add relative dir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"/../jophiel-dist/ansible
 
 ansible --version
 
-ansible-playbook -c local -e @tlx-staging/jophiel_conf.yml playbooks/build-jophiel.yml
-ansible-playbook -c local -e @tlx-staging/jophiel_conf.yml playbooks/build-jophiel-nginx.yml
-ansible-playbook -e @tlx-staging/jophiel_conf.yml playbooks/deploy-jophiel.yml
+ansible-playbook -c local -e @../../tlx-staging/jophiel_conf.yml playbooks/build-jophiel.yml
+ansible-playbook -c local -e @../../tlx-staging/jophiel_conf.yml playbooks/build-jophiel-nginx.yml
+ansible-playbook -e @../../tlx-staging/jophiel_conf.yml playbooks/deploy-jophiel.yml
