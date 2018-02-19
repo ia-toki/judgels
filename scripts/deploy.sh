237d48a9d960e23e646238c58e551a5b61846bb1
Jordan Fernando <fernandojordan.92@gmail.com>
2018-02-19 22:13:25 +0800

    Move config to another repo.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"/../jophiel-dist/ansible
 
 ansible --version
 
-ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel.yml
-ansible-playbook -c local -e dockerhub_token=$dockerhub_token playbooks/build-jophiel-nginx.yml
-ansible-playbook -e db_username=$db_username -e db_password=$db_password playbooks/deploy-jophiel.yml
+ansible-playbook -c local -e @tlx-staging/jophiel_conf.yml playbooks/build-jophiel.yml
+ansible-playbook -c local -e @tlx-staging/jophiel_conf.yml playbooks/build-jophiel-nginx.yml
+ansible-playbook -e @tlx-staging/jophiel_conf.yml playbooks/deploy-jophiel.yml
