93d849f477c669c438233eade2f124f613453ffd
Frederikus Hudi <frederikus.hudi@gmail.com>
2018-02-25 21:48:12 +0900

    Contest detail page (#49)

--- src/routes/labs/LabsRoutes.tsx
+++ src/routes/labs/LabsRoutes.tsx
@@ -3,12 +3,14 @@ import { Route, Switch, withRouter } from 'react-router';
 
 import { Example } from './routes/example/Example/Example';
 import ScoreboardContainer from './routes/scoreboard/ScoreboardContainer/ScoreboardContainer';
+import ContestDetailPage from './routes/contestDetail/ContestDetailPage/ContestDetailPage';
 
 const LabsRoutes = () => (
   <div>
     <Switch>
       <Route exact path="/labs/example" component={Example} />
       <Route exact path="/labs/scoreboard" component={ScoreboardContainer} />
+      <Route exact path="/labs/contestDetail" component={ContestDetailPage} />
     </Switch>
   </div>
 );
