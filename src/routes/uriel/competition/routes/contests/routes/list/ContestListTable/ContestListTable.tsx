4770663f014f8d6db5032e8da290de435da12980
Ashar Fuadi <fushar@gmail.com>
2018-02-27 13:54:54 +0700

    Add link to individual contest scoreboards

--- src/routes/uriel/competition/routes/contests/routes/list/ContestListTable/ContestListTable.tsx
+++ src/routes/uriel/competition/routes/contests/routes/list/ContestListTable/ContestListTable.tsx
@@ -1,9 +1,10 @@
-import { Card, Button, Intent } from '@blueprintjs/core';
+import { Card, Intent } from '@blueprintjs/core';
 import * as React from 'react';
 
 import { ContestList } from '../../../../../../../../modules/api/uriel/contest';
 
 import './ContestListTable.css';
+import { ButtonLink } from '../../../../../../../../components/ButtonLink/ButtonLink';
 
 export interface ContestListTableProps {
   contestList: ContestList;
@@ -12,17 +13,20 @@ export interface ContestListTableProps {
 export class ContestListTable extends React.Component<ContestListTableProps, {}> {
   render() {
     const { contestList } = this.props;
-    const list = contestList.data.map((item, id) => (
-      <div key={id} className="flex-row justify-content-space-between contest-list-item-container">
+    const list = contestList.data.map(contest => (
+      <div key={contest.jid} className="flex-row justify-content-space-between contest-list-item-container">
         <div>
-          <h3>{item.name}</h3>
+          <h3>{contest.name}</h3>
         </div>
         <div className="flex-column contest-list-item-info">
           <div className="flex-row justify-content-flex-end">
-            {/* TODO add on click */}
-            <Button intent={Intent.PRIMARY} className="contest-list-view-result">
+            <ButtonLink
+              to={`/competition/contests/${contest.jid}/scoreboard`}
+              intent={Intent.PRIMARY}
+              className="contest-list-view-result"
+            >
               View scoreboard
-            </Button>
+            </ButtonLink>
           </div>
         </div>
       </div>
