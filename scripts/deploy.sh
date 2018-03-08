28aa74368c693c85aa78bf453cda7951611da1e2
Ashar Fuadi <fushar@gmail.com>
2018-03-08 07:46:13 +0700

    Merge build_artifact script to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"/../jophiel-dist/ansible
+set -ex
+
+cd "$(dirname "$0")"/../jophiel-dist
+
+../gradlew clean distTar
+tar -xf build/distributions/jophiel-* --strip-components=1 -C build/distributions
+rm build/distributions/*.tgz
+mv build/distributions/var/conf/jophiel.yml.example build/distributions/var/conf/jophiel.yml
+
+cp dockerfiles/jophiel/Dockerfile build/
+cp dockerfiles/jophiel/.dockerignore build/
+
+cd ansible
 
 ansible --version
 
