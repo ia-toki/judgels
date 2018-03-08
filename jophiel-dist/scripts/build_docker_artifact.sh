98f3b2e1e24638491b4c6004c76afd0481e24c6c
Ashar Fuadi <fushar@gmail.com>
2018-03-08 07:27:51 +0700

    Simplify docker build task by copying Dockerfile into build/ dir

--- jophiel-dist/scripts/build_docker_artifact.sh
+++ jophiel-dist/scripts/build_docker_artifact.sh
@@ -1,10 +1,12 @@
-set +ex
+#!/bin/bash
 
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)/../jophiel-dist"
-TAG=$1
+set -ex
 
-./gradlew clean distTar
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)/.."
+
+$DIR/../gradlew clean distTar
 tar -xf $DIR/build/distributions/jophiel-* --strip-components=1 -C $DIR/build/distributions
 rm $DIR/build/distributions/*.tgz
 mv $DIR/build/distributions/var/conf/jophiel.yml.example $DIR/build/distributions/var/conf/jophiel.yml
-docker build -t judgels/jophiel:$TAG $DIR
+cp $DIR/dockerfiles/jophiel/Dockerfile $DIR/build/
+cp $DIR/dockerfiles/jophiel/.dockerignore $DIR/build/
