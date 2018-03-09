ac1ae0292019d98ef6f9a22f6dbc978e0f8295c8
Ashar Fuadi <fushar@gmail.com>
2018-03-09 16:20:50 +0700

    Pass deployment repo from travis env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,10 +13,10 @@ cp dockerfiles/jophiel/.dockerignore build/
 
 cd ansible
 ansible --version
-cp ../../tlx-staging/global/hosts .
+cp ../../deployment/global/hosts .
 
 mkdir -p roles/jophiel-deploy/templates
-cp ../../tlx-staging/jophiel/conf/jophiel.yml roles/jophiel-deploy/templates/jophiel.yml.j2
+cp ../../deployment/jophiel/conf/jophiel.yml roles/jophiel-deploy/templates/jophiel.yml.j2
 
-ansible-playbook -c local -e @../../tlx-staging/global/env.yml playbooks/build-jophiel.yml
-ansible-playbook -e @../../tlx-staging/global/env.yml playbooks/deploy-jophiel.yml
+ansible-playbook -c local -e @../../deployment/global/env.yml playbooks/build-jophiel.yml
+ansible-playbook -e @../../deployment/global/env.yml playbooks/deploy-jophiel.yml
