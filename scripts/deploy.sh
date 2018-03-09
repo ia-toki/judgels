fd5394c565b218e3827bee09e8a39f641efc73a3
Ashar Fuadi <fushar@gmail.com>
2018-03-09 16:00:29 +0700

    Move jophiel-nginx roles and playbooks to tlx-staging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,19 +7,16 @@ cd "$(dirname "$0")"/../jophiel-dist
 ../gradlew clean distTar
 tar -xf build/distributions/jophiel-* --strip-components=1 -C build/distributions
 rm build/distributions/*.tgz
-mv build/distributions/var/conf/jophiel.yml.example build/distributions/var/conf/jophiel.yml
 
 cp dockerfiles/jophiel/Dockerfile build/
 cp dockerfiles/jophiel/.dockerignore build/
 
 cd ansible
 ansible --version
-cp ../../tlx-staging/jophiel/ansible/* .
+cp ../../tlx-staging/global/hosts .
 
 mkdir -p roles/jophiel-deploy/templates
 cp ../../tlx-staging/jophiel/conf/jophiel.yml roles/jophiel-deploy/templates/jophiel.yml.j2
-ansible-playbook -c local -e @../../tlx-staging/global.yml playbooks/build-jophiel.yml
 
-ansible-playbook -c local -e @../../tlx-staging/global.yml playbooks/build-jophiel-nginx.yml
-
-ansible-playbook -e @../../tlx-staging/global.yml playbooks/deploy-jophiel.yml
+ansible-playbook -c local -e @../../tlx-staging/global/env.yml playbooks/build-jophiel.yml
+ansible-playbook -e @../../tlx-staging/global/env.yml playbooks/deploy-jophiel.yml
