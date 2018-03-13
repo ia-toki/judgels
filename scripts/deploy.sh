5267580d53cbb39102323fedda5fb0f63f4f64be
Ashar Fuadi <fushar@gmail.com>
2018-03-13 20:36:14 +0700

    Update deployment script to match the ones in judgels-dev/judgels

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,11 @@ cd "$(dirname "$0")"/../jophiel-dist
 tar -xf build/distributions/jophiel-* --strip-components=1 -C build/distributions
 rm build/distributions/*.tgz
 
-cd ansible
 ansible --version
-cp ../../deployment/global/hosts .
 
-mkdir -p roles/jophiel-deploy/templates
-cp ../../deployment/jophiel/conf/jophiel.yml roles/jophiel-deploy/templates/jophiel.yml.j2
+cd ansible
+ansible-playbook -e @../../deployment/conf/global.yml playbooks/build-jophiel.yml
 
-ansible-playbook -c local -e @../../deployment/global/env.yml playbooks/build-jophiel.yml
-ansible-playbook -e @../../deployment/global/env.yml playbooks/deploy-jophiel.yml
+cd ../../judgels/ansible
+cp ../../deployment/conf/jophiel.yml roles/jophiel-deploy/templates/jophiel.yml.j2
+ansible-playbook -e @../../deployment/conf/global.yml playbooks/deploy-jophiel.yml
