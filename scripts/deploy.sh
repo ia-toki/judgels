22898d3a649728a79fdfb8ce4bd3a1fd6b5ccf0a
Ashar Fuadi <fushar@gmail.com>
2018-03-20 16:54:40 +0700

    No need to copy raphael.js.j2 anymore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,5 +13,4 @@ cd dist/ansible
 ansible-playbook -e @../../deployment/conf/global.yml playbooks/build-raphael.yml
 
 cd ../../judgels/ansible
-cp ../../deployment/conf/raphael.js roles/raphael-deploy/templates/raphael.js.j2
 ansible-playbook -e @../../deployment/conf/global.yml playbooks/deploy-raphael.yml
