9d6ac21600afcdbb9cc590b50bb7ae677375458a
Ashar Fuadi <fushar@gmail.com>
2018-04-05 09:42:39 +0700

    Add ansible deployment

--- scripts/build_docker_image.sh
+++
@@ -1,9 +0,0 @@
-set +ex
-
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)/../sealtiel-dist"
-TAG=$1
-
-./gradlew clean distTar
-tar -xf $DIR/build/distributions/sealtiel-* -C $DIR/build/distributions
-rm $DIR/build/distributions/*.tgz
-docker build -t judgels/sealtiel:$TAG $DIR
