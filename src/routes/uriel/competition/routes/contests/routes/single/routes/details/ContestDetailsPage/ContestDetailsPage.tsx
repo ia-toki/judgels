e46d7595b6a171fc08afdafb6edac3c4a0419509
Ashar Fuadi <fushar@gmail.com>
2018-04-06 04:01:35 +0700

    Show link to legacy contest page in details page

--- src/routes/uriel/competition/routes/contests/routes/single/routes/details/ContestDetailsPage/ContestDetailsPage.tsx
+++ src/routes/uriel/competition/routes/contests/routes/single/routes/details/ContestDetailsPage/ContestDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@blueprintjs/core';
+import { Callout, Card, Intent } from '@blueprintjs/core';
 import * as HTMLReactParser from 'html-react-parser';
 import * as React from 'react';
 import { connect } from 'react-redux';
@@ -7,6 +7,7 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { Contest } from '../../../../../../../../../../modules/api/uriel/contest';
 import { AppState } from '../../../../../../../../../../modules/store';
 import { selectContest } from '../../../../../modules/contestSelectors';
+import { APP_CONFIG } from '../../../../../../../../../../conf';
 
 interface ContestDetailsPageProps extends RouteComponentProps<{ contestJid: string }> {
   contest: Contest;
@@ -16,7 +17,18 @@ class ContestDetailsPage extends React.Component<ContestDetailsPageProps> {
   render() {
     const { contest } = this.props;
 
-    return <Card>{HTMLReactParser(contest.description)}</Card>;
+    return (
+      <div>
+        <Callout intent={Intent.WARNING} iconName="info-sign">
+          Under construction. See the old page here:{' '}
+          <a href={`${APP_CONFIG.tempHome.urielUrl}/contests/${contest.id}`}>
+            {APP_CONFIG.tempHome.urielUrl}/contests/{contest.id}
+          </a>
+        </Callout>
+        <hr />
+        <Card>{HTMLReactParser(contest.description)}</Card>
+      </div>
+    );
   }
 }
 
