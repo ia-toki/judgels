5052125813d870dca77e93f1841a980793aa67c7
Ashar Fuadi <fushar@gmail.com>
2018-04-10 14:44:59 +0700

    Add updateAll()

--- judgels-persistence-core/src/main/java/judgels/persistence/Dao.java
+++ judgels-persistence-core/src/main/java/judgels/persistence/Dao.java
@@ -1,5 +1,8 @@
 package judgels.persistence;
 
+import java.util.List;
+
 public interface Dao<M extends Model> extends UnmodifiableDao<M> {
     M update(M model);
+    List<M> updateAll(List<M> models);
 }
