3dc0f4f06eba09b059c1da83c8111d5a2ef206d5
Ashar Fuadi <fushar@gmail.com>
2018-04-18 12:54:15 +0700

    Add or() convenience method in CustomPredicateFilter

--- judgels-persistence-core/src/main/java/judgels/persistence/CustomPredicateFilter.java
+++ judgels-persistence-core/src/main/java/judgels/persistence/CustomPredicateFilter.java
@@ -7,4 +7,14 @@ import javax.persistence.criteria.Root;
 
 public interface CustomPredicateFilter<M> {
     Predicate apply(CriteriaBuilder cb, CriteriaQuery<?> cq, Root<M> root);
+
+    static <M> CustomPredicateFilter<M> or(CustomPredicateFilter<M>... predicateFilters) {
+        return (cb, cq, root) -> {
+            Predicate[] predicates = new Predicate[predicateFilters.length];
+            for (int i = 0; i < predicateFilters.length; i++) {
+                predicates[i] = predicateFilters[i].apply(cb, cq, root);
+            }
+            return cb.or(predicates);
+        };
+    }
 }
