26d67f8f0d229bfedb0cedc92d165e720d5c5c35
Ashar Fuadi <fushar@gmail.com>
2018-04-27 22:02:24 +0700

    Add JudgelsObjectMappers

---
+++ judgels-service-core/src/main/java/judgels/service/jersey/JudgelsObjectMappers.java
@@ -0,0 +1,14 @@
+package judgels.service.jersey;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JudgelsObjectMappers {
+    private JudgelsObjectMappers() {}
+
+    public static void configure(ObjectMapper mapper) {
+        mapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+        mapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
+    }
+}
