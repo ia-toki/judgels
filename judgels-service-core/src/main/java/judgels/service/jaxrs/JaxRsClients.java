f9332d2f67c614a96080700c67616675a8e914eb
Ashar Fuadi <fushar@gmail.com>
2018-04-28 19:50:14 +0700

    Hack http-remoting to configure object mapper

--- judgels-service-core/src/main/java/judgels/service/jaxrs/JaxRsClients.java
+++ judgels-service-core/src/main/java/judgels/service/jaxrs/JaxRsClients.java
@@ -1,10 +1,32 @@
 package judgels.service.jaxrs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.palantir.remoting3.clients.ClientConfiguration;
 import com.palantir.remoting3.clients.ClientConfigurations;
 import com.palantir.remoting3.clients.UserAgent;
-import com.palantir.remoting3.jaxrs.JaxRsClient;
+import com.palantir.remoting3.ext.jackson.ObjectMappers;
+import com.palantir.remoting3.jaxrs.feignimpl.Java8OptionalAwareContract;
+import com.palantir.remoting3.jaxrs.feignimpl.PathTemplateHeaderEnrichmentContract;
+import com.palantir.remoting3.jaxrs.feignimpl.PathTemplateHeaderRewriter;
+import com.palantir.remoting3.jaxrs.feignimpl.SlashEncodingContract;
+import com.palantir.remoting3.okhttp.OkHttpClients;
+import feign.Contract;
+import feign.Feign;
+import feign.InputStreamDelegateDecoder;
+import feign.InputStreamDelegateEncoder;
+import feign.Java8OptionalAwareDecoder;
+import feign.Logger;
+import feign.Request;
+import feign.Retryer;
+import feign.TextDelegateDecoder;
+import feign.TextDelegateEncoder;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.jaxrs.JAXRSContract;
+import feign.okhttp.OkHttpClient;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -16,6 +38,7 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
+import judgels.service.jersey.JudgelsObjectMappers;
 
 public class JaxRsClients {
     private static X509TrustManager defaultTrustManager;
@@ -46,9 +69,47 @@ public class JaxRsClients {
 
     private JaxRsClients() {}
 
+    // Adapted from com.palantir.remoting3.jaxrs.AbstractFeignJaxRsClientBuilder
     public static <T> T create(Class<T> serviceClass, String uri, UserAgent userAgent) {
         List<String> uris = ImmutableList.of(uri);
         ClientConfiguration clientConfig = ClientConfigurations.of(uris, defaultSslSocketFactory, defaultTrustManager);
-        return JaxRsClient.create(serviceClass, userAgent, clientConfig);
+        ObjectMapper objectMapper = JudgelsObjectMappers.configure(ObjectMappers.newClientObjectMapper());
+
+        return Feign.builder()
+                .contract(createContract())
+                .encoder(createEncoder(objectMapper))
+                .decoder(createDecoder(objectMapper))
+                .requestInterceptor(PathTemplateHeaderRewriter.INSTANCE)
+                .client(new OkHttpClient(OkHttpClients.create(clientConfig, userAgent, serviceClass)))
+                .options(createRequestOptions(clientConfig))
+                .logLevel(Logger.Level.NONE)  // we use OkHttp interceptors for logging. (note that NONE is the default)
+                .retryer(new Retryer.Default(0, 0, 1))  // use OkHttp retry mechanism only
+                .target(serviceClass, uri);
+    }
+
+    private static Contract createContract() {
+        return new PathTemplateHeaderEnrichmentContract(
+                new SlashEncodingContract(
+                        new Java8OptionalAwareContract(
+                                new JAXRSContract())));
+    }
+
+    private static Request.Options createRequestOptions(ClientConfiguration clientConfig) {
+        return new Request.Options(
+                Math.toIntExact(clientConfig.connectTimeout().toMillis()),
+                Math.toIntExact(clientConfig.readTimeout().toMillis()));
+    }
+
+    private static Encoder createEncoder(ObjectMapper objectMapper) {
+        return new InputStreamDelegateEncoder(
+                new TextDelegateEncoder(
+                        new JacksonEncoder(objectMapper)));
+    }
+
+    private static Decoder createDecoder(ObjectMapper objectMapper) {
+        return new Java8OptionalAwareDecoder(
+                new InputStreamDelegateDecoder(
+                        new TextDelegateDecoder(
+                                new JacksonDecoder(objectMapper))));
     }
 }
--- judgels-service-core/src/main/java/judgels/service/jersey/JudgelsObjectMappers.java
+++ judgels-service-core/src/main/java/judgels/service/jersey/JudgelsObjectMappers.java
@@ -7,8 +7,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 public class JudgelsObjectMappers {
     private JudgelsObjectMappers() {}
 
-    public static void configure(ObjectMapper mapper) {
-        mapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
-        mapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
+    public static ObjectMapper configure(ObjectMapper mapper) {
+        mapper
+                .disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
+                .disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
+
+        return mapper;
     }
 }
