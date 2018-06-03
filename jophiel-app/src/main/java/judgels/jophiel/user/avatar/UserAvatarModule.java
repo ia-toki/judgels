a18546aac88732b13edc061778fb3e23be0b5f1a
Ashar Fuadi <fushar@gmail.com>
2018-06-03 13:02:26 +0700

    Use convenience class FileSystems

--- jophiel-app/src/main/java/judgels/jophiel/user/avatar/UserAvatarModule.java
+++ jophiel-app/src/main/java/judgels/jophiel/user/avatar/UserAvatarModule.java
@@ -2,16 +2,12 @@ package judgels.jophiel.user.avatar;
 
 import dagger.Module;
 import dagger.Provides;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 import javax.inject.Singleton;
 import judgels.fs.FileSystem;
+import judgels.fs.FileSystems;
 import judgels.fs.aws.AwsConfiguration;
-import judgels.fs.aws.AwsFileSystem;
-import judgels.fs.aws.AwsFsConfiguration;
-import judgels.fs.local.LocalFileSystem;
 
 @Module
 public class UserAvatarModule {
@@ -27,16 +23,6 @@ public class UserAvatarModule {
     @Singleton
     @UserAvatarFs
     FileSystem userAvatarFs(Optional<AwsConfiguration> awsConfig) {
-        if (config.getFs() instanceof AwsFsConfiguration) {
-            return new AwsFileSystem(awsConfig.get(), (AwsFsConfiguration) config.getFs());
-        } else {
-            Path baseDir = baseDataDir.resolve("user-avatars");
-            try {
-                Files.createDirectories(baseDir);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return new LocalFileSystem(baseDir);
-        }
+        return FileSystems.get(config.getFs(), awsConfig, baseDataDir.resolve("user-avatars"));
     }
 }
