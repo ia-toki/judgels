c9285d44b38c8b75c7badaa9f99088567265e7b6
Ashar Fuadi <fushar@gmail.com>
2018-06-03 13:31:12 +0700

    Use convencience FileSystems class

--- uriel-app/src/main/java/judgels/uriel/submission/SubmissionModule.java
+++ uriel-app/src/main/java/judgels/uriel/submission/SubmissionModule.java
@@ -2,16 +2,12 @@ package judgels.uriel.submission;
 
 import dagger.Module;
 import dagger.Provides;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 import javax.inject.Singleton;
 import judgels.fs.FileSystem;
+import judgels.fs.FileSystems;
 import judgels.fs.aws.AwsConfiguration;
-import judgels.fs.aws.AwsFileSystem;
-import judgels.fs.aws.AwsFsConfiguration;
-import judgels.fs.local.LocalFileSystem;
 
 @Module
 public class SubmissionModule {
@@ -27,16 +23,6 @@ public class SubmissionModule {
     @Singleton
     @SubmissionFs
     FileSystem submissionFs(Optional<AwsConfiguration> awsConfig) {
-        if (config.getFs() instanceof AwsFsConfiguration) {
-            return new AwsFileSystem(awsConfig.get(), (AwsFsConfiguration) config.getFs());
-        } else {
-            Path baseDir = baseDataDir.resolve("submissions");
-            try {
-                Files.createDirectories(baseDir);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return new LocalFileSystem(baseDir);
-        }
+        return FileSystems.get(config.getFs(), awsConfig, baseDataDir.resolve("submissions"));
     }
 }
