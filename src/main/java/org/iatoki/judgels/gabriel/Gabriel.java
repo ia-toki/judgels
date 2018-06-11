2cb911bb283f8f59b6fa5c678719070a41bbb65c
Ashar Fuadi <fushar@gmail.com>
2018-06-11 10:00:27 +0700

    Serde byte array as base 64 encoded string

--- src/main/java/org/iatoki/judgels/gabriel/Gabriel.java
+++ src/main/java/org/iatoki/judgels/gabriel/Gabriel.java
@@ -1,6 +1,10 @@
 package org.iatoki.judgels.gabriel;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
 import com.google.gson.JsonSyntaxException;
 import org.iatoki.judgels.api.JudgelsAPIClientException;
 import org.iatoki.judgels.api.sandalphon.SandalphonClientAPI;
@@ -9,6 +13,7 @@ import org.iatoki.judgels.api.sealtiel.SealtielClientAPI;
 import org.iatoki.judgels.api.sealtiel.SealtielFactory;
 import org.iatoki.judgels.api.sealtiel.SealtielMessage;
 
+import java.util.Base64;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -68,7 +73,12 @@ public final class Gabriel {
 
     private void processMessage(SealtielMessage message) {
         try {
-            GradingRequest request = new Gson().fromJson(message.getMessage(), GradingRequest.class);
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapter(byte[].class, (JsonSerializer<byte[]>) (src, typeOfSrc, context) -> new JsonPrimitive(Base64.getEncoder().encodeToString(src)));
+            builder.registerTypeAdapter(byte[].class, (JsonDeserializer<byte[]>) (json, typeOfT, context) -> Base64.getDecoder().decode(json.getAsString()));
+            Gson gson = builder.create();
+
+            GradingRequest request = gson.fromJson(message.getMessage(), GradingRequest.class);
 
             GabrielLogger.getLogger().info("New grading request: {}", request.getGradingJid());
 
