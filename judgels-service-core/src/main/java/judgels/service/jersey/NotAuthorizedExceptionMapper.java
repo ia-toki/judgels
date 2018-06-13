411a582a187d2393102bcf967896ce5189e1c450
Ashar Fuadi <fushar@gmail.com>
2018-06-13 11:43:54 +0700

    Add errorInstanceId when 401

--- judgels-service-core/src/main/java/judgels/service/jersey/NotAuthorizedExceptionMapper.java
+++ judgels-service-core/src/main/java/judgels/service/jersey/NotAuthorizedExceptionMapper.java
@@ -2,6 +2,7 @@ package judgels.service.jersey;
 
 import com.google.common.collect.Maps;
 import java.util.Map;
+import java.util.UUID;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,6 +18,7 @@ public class NotAuthorizedExceptionMapper implements ExceptionMapper<NotAuthoriz
         Map<String, String> serializableError = Maps.newHashMap();
         serializableError.put("errorCode", "CUSTOM_CLIENT");
         serializableError.put("errorName", "Judgels:Unauthorized");
+        serializableError.put("errorInstanceId", UUID.randomUUID().toString());
 
         return Response
                 .status(Response.Status.UNAUTHORIZED)
