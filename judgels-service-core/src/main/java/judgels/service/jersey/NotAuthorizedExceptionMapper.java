e52678834a4b00f1df882b0a0e8bd5dcb64e55f9
Ashar Fuadi <fushar@gmail.com>
2018-06-13 11:04:39 +0700

    Throw 401 as SerializableError

--- judgels-service-core/src/main/java/judgels/service/jersey/NotAuthorizedExceptionMapper.java
+++ judgels-service-core/src/main/java/judgels/service/jersey/NotAuthorizedExceptionMapper.java
@@ -1,6 +1,9 @@
 package judgels.service.jersey;
 
+import com.google.common.collect.Maps;
+import java.util.Map;
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -9,6 +12,16 @@ import javax.ws.rs.ext.Provider;
 public class NotAuthorizedExceptionMapper implements ExceptionMapper<NotAuthorizedException> {
     @Override
     public Response toResponse(NotAuthorizedException exception) {
-        return Response.status(Response.Status.UNAUTHORIZED).build();
+        // This is a hack because currently http-remoting does not support 401 exception :(
+
+        Map<String, String> serializableError = Maps.newHashMap();
+        serializableError.put("errorType", "CUSTOM_CLIENT");
+        serializableError.put("errorName", "Judgels:Unauthorized");
+
+        return Response
+                .status(Response.Status.UNAUTHORIZED)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(serializableError)
+                .build();
     }
 }
