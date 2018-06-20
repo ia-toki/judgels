f762724681233f7af2ff3f3022162cf7c7a4d817
Ashar Fuadi <fushar@gmail.com>
2018-06-20 15:08:28 +0700

    Implement caching for actor checks

--- uriel-app/src/main/java/judgels/uriel/UrielModule.java
+++ uriel-app/src/main/java/judgels/uriel/UrielModule.java
@@ -3,16 +3,14 @@ package judgels.uriel;
 import static judgels.uriel.VersionResource.VERSION;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.palantir.remoting.api.errors.RemoteException;
 import com.palantir.remoting3.clients.UserAgent;
 import com.palantir.remoting3.ext.jackson.ObjectMappers;
 import dagger.Module;
 import dagger.Provides;
-import java.util.Optional;
 import javax.inject.Singleton;
-import javax.ws.rs.NotAuthorizedException;
 import judgels.jophiel.api.user.MyService;
 import judgels.service.actor.ActorChecker;
+import judgels.service.actor.CachingActorExtractor;
 import judgels.service.jersey.JudgelsObjectMappers;
 
 @Module
@@ -35,15 +33,6 @@ public class UrielModule {
     @Provides
     @Singleton
     static ActorChecker actorChecker(MyService myService) {
-        return new ActorChecker(authHeader -> {
-            try {
-                return Optional.of(myService.getMyself(authHeader).getJid());
-            } catch (RemoteException e) {
-                if (e.getStatus() == 401) {
-                    throw new NotAuthorizedException(e);
-                }
-            }
-            return Optional.empty();
-        });
+        return new ActorChecker(new CachingActorExtractor(myService));
     }
 }
