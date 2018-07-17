c2be2a1c332fc26a8ebf0aa2b7406b9f19ca7311
Ashar Fuadi <fushar@gmail.com>
2018-07-17 21:42:06 +0700

    Remove original scripts/

--- scripts/build_docker_image.sh
+++
@@ -1,8 +0,0 @@
-set +ex
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
-TAG=$1
-
-GENERATE_SOURCEMAP=false yarn build
-mv $DIR/build/var/conf/raphael.js.example $DIR/build/var/conf/raphael.js
-docker build -t judgels/raphael:$TAG $DIR
