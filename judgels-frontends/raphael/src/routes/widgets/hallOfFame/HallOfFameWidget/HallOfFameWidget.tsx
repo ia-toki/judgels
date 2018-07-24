1e9c1ec25e0b3c47791bff404be5873678bdf099
Ashar Fuadi <fushar@gmail.com>
2018-07-24 20:45:13 +0700

    Add temporary hall of fame widget

---
+++ judgels-frontends/raphael/src/routes/widgets/hallOfFame/HallOfFameWidget/HallOfFameWidget.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { Card } from '../../../../components/Card/Card';
+
+class HallOfFameWidget extends React.PureComponent {
+  render() {
+    return (
+      <Card title="Hall of fame">
+        <small>No data yet.</small>
+      </Card>
+    );
+  }
+}
+
+export default HallOfFameWidget;
