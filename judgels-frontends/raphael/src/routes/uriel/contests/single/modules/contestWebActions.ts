187c3b990a828bec5d44a27cb480c01ecae0b4b6
Ashar Fuadi <fushar@gmail.com>
2018-08-20 08:07:47 +0700

    Forgotten contest cache invalidation

--- judgels-frontends/raphael/src/routes/uriel/contests/single/modules/contestWebActions.ts
+++ judgels-frontends/raphael/src/routes/uriel/contests/single/modules/contestWebActions.ts
@@ -1,14 +1,16 @@
 import { selectToken } from 'modules/session/sessionSelectors';
 
+import { PutContest } from '../../modules/contestReducer';
 import { DelWebConfig, PutWebConfig } from '../../modules/contestWebConfigReducer';
 
 export const contestWebActions = {
   getContestBySlugWithWebConfig: (contestJid: string) => {
     return async (dispatch, getState, { contestWebAPI }) => {
       const token = selectToken(getState());
-      const contestWithConfig = await contestWebAPI.getContestBySlugWithWebConfig(token, contestJid);
-      dispatch(PutWebConfig.create(contestWithConfig.config));
-      return contestWithConfig;
+      const { contest, config } = await contestWebAPI.getContestBySlugWithWebConfig(token, contestJid);
+      dispatch(PutContest.create(contest));
+      dispatch(PutWebConfig.create(config));
+      return { contest, config };
     };
   },
 
