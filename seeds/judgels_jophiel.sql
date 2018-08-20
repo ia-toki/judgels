1d3838ea3329f0275a56f3bce83870ba51e908ca
Ashar Fuadi <fushar@gmail.com>
2018-08-20 17:22:51 +0700

    Add fake problems and scoreboards to seed data

--- seeds/judgels_jophiel.sql
+++ seeds/judgels_jophiel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 05, 2018 at 06:38 PM
+-- Generation Time: Aug 20, 2018 at 05:02 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -28,24 +28,24 @@ SET time_zone = "+00:00";
 
 INSERT INTO `jophiel_user` (`jid`, `username`, `email`, `password`, `avatarFilename`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
 ('JIDUSERGBsbuD2EJ9jYMlGclISJ', 'superadmin', 'superadmin@jophiel.judgels', '1000:ffe7adbe30cf950f3dfaf7036ec80cd30dc19f034a8301bb:51452bbf71298b8090fbf07dbfc945386bcc508f83a29b99', NULL, '2018-08-05 16:37:57.305', NULL, NULL, '2018-08-05 16:37:57.305', NULL, NULL),
-('JIDUSER7uMucIkm1exJTu7sJvxR', 'andi', 'andi@jophiel.judgels', '1000:060ce62731682f2a43de103418b1afad8af98bd6f399b569:cbe47b1335de632a21c7a4b56e04214b7c3a349d120867b3', NULL, '2018-08-05 17:03:33.504', NULL, '127.0.0.1', '2018-08-05 17:11:29.127', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERDMZOs8UHqjsw9DGbB31z', 'budi', 'budi@jophiel.judgels', '1000:cdbd48d2ab492347a83ba0e8351ff4068f8109d7f77a371a:277f5b263e855fcd59cf3006f7b1c5bfac3de02ba24452f5', NULL, '2018-08-05 17:15:23.586', 'JIDUSER7uMucIkm1exJTu7sJvxR', '127.0.0.1', '2018-08-05 17:15:23.586', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERVfszUmquMh0Ae2laygDp', 'caca', 'caca@jophiel.judgels', '1000:f6d2c169f4ed162e7078065e3c83baee8e07c80eb4c3ab19:66660990bc19491f9b60e6bf0912d74da45c94da524750b4', NULL, '2018-08-05 17:16:27.740', 'JIDUSERDMZOs8UHqjsw9DGbB31z', '127.0.0.1', '2018-08-05 17:16:27.740', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSER5QiWnVJWzKdjtWtgEfN3', 'dudi', 'dudi@jophiel.judgels', '1000:f35e1730d05f8c600a64ac56ecf609d639471dffa3223be4:c176c95c192f52a93d49c14f2cd9506b737a8e4188aee621', NULL, '2018-08-05 17:16:27.740', 'JIDUSERDMZOs8UHqjsw9DGbB31z', '127.0.0.1', '2018-08-05 17:16:27.740', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERXCcE36qmrr8Tm46k3hSV', 'emir', 'emir@jophiel.judgels', '1000:72f3edcab574f0b1b814dd1fb761ad8a8d1bfe6298f0354f:cec5a264c4bd34da306271aead1678155577b75a61c6579c', NULL, '2018-08-05 17:16:27.740', 'JIDUSERDMZOs8UHqjsw9DGbB31z', '127.0.0.1', '2018-08-05 17:16:27.740', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERP1gi91MNc1XbXLbPx3dq', 'fuad', 'fuad@jophiel.judgels', '1000:4da2460d8838a109ef8dde5f8183e6d958e2a4a43a0bb578:9cf73cfec1faea4b651cfdb1bafd6f5e3a25fb6385166dc6', NULL, '2018-08-05 17:16:27.740', 'JIDUSERDMZOs8UHqjsw9DGbB31z', '127.0.0.1', '2018-08-05 17:16:27.740', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1');
+('JIDUSER7uMucIkm1exJTu7sJvxR', 'andi', 'andi@jophiel.judgels', '1000:060ce62731682f2a43de103418b1afad8af98bd6f399b569:cbe47b1335de632a21c7a4b56e04214b7c3a349d120867b3', NULL, '2018-08-05 17:03:33.504', NULL, NULL, '2018-08-05 17:11:29.127', NULL, NULL),
+('JIDUSERDMZOs8UHqjsw9DGbB31z', 'budi', 'budi@jophiel.judgels', '1000:cdbd48d2ab492347a83ba0e8351ff4068f8109d7f77a371a:277f5b263e855fcd59cf3006f7b1c5bfac3de02ba24452f5', NULL, '2018-08-05 17:15:23.586', NULL, NULL, '2018-08-05 17:15:23.586', NULL, NULL),
+('JIDUSERVfszUmquMh0Ae2laygDp', 'caca', 'caca@jophiel.judgels', '1000:f6d2c169f4ed162e7078065e3c83baee8e07c80eb4c3ab19:66660990bc19491f9b60e6bf0912d74da45c94da524750b4', NULL, '2018-08-05 17:16:27.740', NULL, NULL, '2018-08-05 17:16:27.740', NULL, NULL),
+('JIDUSER5QiWnVJWzKdjtWtgEfN3', 'dudi', 'dudi@jophiel.judgels', '1000:f35e1730d05f8c600a64ac56ecf609d639471dffa3223be4:c176c95c192f52a93d49c14f2cd9506b737a8e4188aee621', NULL, '2018-08-05 17:16:27.740', NULL, NULL, '2018-08-05 17:16:27.740', NULL, NULL),
+('JIDUSERXCcE36qmrr8Tm46k3hSV', 'emir', 'emir@jophiel.judgels', '1000:72f3edcab574f0b1b814dd1fb761ad8a8d1bfe6298f0354f:cec5a264c4bd34da306271aead1678155577b75a61c6579c', NULL, '2018-08-05 17:16:27.740', NULL, NULL, '2018-08-05 17:16:27.740', NULL, NULL),
+('JIDUSERP1gi91MNc1XbXLbPx3dq', 'fuad', 'fuad@jophiel.judgels', '1000:4da2460d8838a109ef8dde5f8183e6d958e2a4a43a0bb578:9cf73cfec1faea4b651cfdb1bafd6f5e3a25fb6385166dc6', NULL, '2018-08-05 17:16:27.740', NULL, NULL, '2018-08-05 17:16:27.740', NULL, NULL);
 
 --
 -- Dumping data for table `jophiel_user_info`
 --
 
 INSERT INTO `jophiel_user_info` (`userJid`, `name`, `gender`, `country`, `homeAddress`, `shirtSize`, `institutionName`, `institutionCountry`, `institutionProvince`, `institutionCity`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
-('JIDUSER7uMucIkm1exJTu7sJvxR', 'Andi', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:03:33.565', NULL, '127.0.0.1', '2018-08-05 17:14:56.338', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERDMZOs8UHqjsw9DGbB31z', 'Budi', 'MALE', 'US', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:15:23.591', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:16:07.734', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERVfszUmquMh0Ae2laygDp', 'Caca', 'FEMALE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:18:18.115', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSER5QiWnVJWzKdjtWtgEfN3', 'Dudi', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:18:18.115', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERXCcE36qmrr8Tm46k3hSV', 'Emir', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:18:18.115', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDUSERP1gi91MNc1XbXLbPx3dq', 'Fuad', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:18:18.115', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1');
+('JIDUSER7uMucIkm1exJTu7sJvxR', 'Andi', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:03:33.565', NULL, NULL, '2018-08-05 17:14:56.338', NULL, NULL),
+('JIDUSERDMZOs8UHqjsw9DGbB31z', 'Budi', 'MALE', 'US', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:15:23.591', NULL, NULL, '2018-08-05 17:16:07.734', NULL, NULL),
+('JIDUSERVfszUmquMh0Ae2laygDp', 'Caca', 'FEMALE', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', NULL, NULL, '2018-08-05 17:18:18.115', NULL, NULL),
+('JIDUSER5QiWnVJWzKdjtWtgEfN3', 'Dudi', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', NULL, NULL, '2018-08-05 17:18:18.115', NULL, NULL),
+('JIDUSERXCcE36qmrr8Tm46k3hSV', 'Emir', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', NULL, NULL, '2018-08-05 17:18:18.115', NULL, NULL),
+('JIDUSERP1gi91MNc1XbXLbPx3dq', 'Fuad', 'MALE', 'ID', NULL, NULL, NULL, NULL, NULL, NULL, '2018-08-05 17:16:27.746', NULL, NULL, '2018-08-05 17:18:18.115', NULL, NULL);
 
 --
 -- Dumping data for table `jophiel_user_rating`
--- seeds/judgels_uriel.sql
+++ seeds/judgels_uriel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 05, 2018 at 07:30 PM
+-- Generation Time: Aug 20, 2018 at 05:05 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -26,61 +26,88 @@ SET time_zone = "+00:00";
 -- Dumping data for table `uriel_contest`
 --
 
-INSERT INTO `uriel_contest` (`jid`, `slug`, `name`, `style`, `description`, `beginTime`, `duration`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`, `locked`) VALUES
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'joc-1', 'Judgels Open Contest #1', 'ICPC', '<p>Welcome to the first Judgels open contest!</p>\r\n', '2018-08-01 12:00:00.000', 18000000000, '2018-08-05 17:30:35.297', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 18:24:26.259', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', b'0'),
-('JIDCONT89YMatMq7GqaPQCyyUHU', 'joc-old', 'Judgels Old Contest', 'IOI', '<p>This is an old contest.</p>\r\n', '2018-01-01 00:00:00.000', 18000000, '2018-08-05 18:11:40.024', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 18:12:03.575', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', b'0');
+INSERT INTO `uriel_contest` (`jid`, `slug`, `name`, `style`, `description`, `beginTime`, `duration`, `locked`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'joc-1', 'Judgels Open Contest #1', 'ICPC', '<p>Welcome to the first Judgels open contest!</p>\r\n', '2018-08-01 12:00:00.000', 18000000000, b'0', '2018-08-05 17:30:35.297', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:15:28.710', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'joc-old', 'Judgels Old Contest', 'IOI', '<p>This is an old contest.</p>\r\n', '2018-01-01 00:00:00.000', 18000000, b'0', '2018-08-05 18:11:40.024', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:35:50.263', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
 
 --
 -- Dumping data for table `uriel_contest_announcement`
 --
 
 INSERT INTO `uriel_contest_announcement` (`jid`, `contestJid`, `title`, `content`, `status`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
-('JIDCOANOtLvAkjKzbAw3vWaZI3t', 'JIDCONTYsWkzwPy8wCF3maJfxqN', 'Good luck', '<p>And have fun!</p>\r\n', 'PUBLISHED', '2018-08-05 17:39:58.816', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:39:58.816', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1');
+('JIDCOANOtLvAkjKzbAw3vWaZI3t', 'JIDCONTYsWkzwPy8wCF3maJfxqN', 'Good luck', '<p>And have fun!</p>\r\n', 'PUBLISHED', '2018-08-05 17:39:58.816', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 17:39:58.816', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
 
 --
 -- Dumping data for table `uriel_contest_clarification`
 --
 
 INSERT INTO `uriel_contest_clarification` (`jid`, `contestJid`, `topicJid`, `title`, `question`, `answer`, `status`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
-('JIDCOCLskqUDQZdvWIRbKgNDFFP', 'JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDCONTYsWkzwPy8wCF3maJfxqN', 'Lunch', 'Will lunch be provided?', 'Yes.', 'ANSWERED', '2018-08-05 18:24:04.866', 'JIDUSER7uMucIkm1exJTu7sJvxR', '127.0.0.1', '2018-08-05 18:24:26.258', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1');
+('JIDCOCLskqUDQZdvWIRbKgNDFFP', 'JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDCONTYsWkzwPy8wCF3maJfxqN', 'Lunch', 'Will lunch be provided?', 'Yes.', 'ANSWERED', '2018-08-05 18:24:04.866', 'JIDUSER7uMucIkm1exJTu7sJvxR', NULL, '2018-08-05 18:24:26.258', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
 
 --
 -- Dumping data for table `uriel_contest_contestant`
 --
 
 INSERT INTO `uriel_contest_contestant` (`contestJid`, `userJid`, `contestStartTime`, `status`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSERVfszUmquMh0Ae2laygDp', NULL, 'APPROVED', '2018-08-05 17:32:08.186', 'JIDUSERVfszUmquMh0Ae2laygDp', '127.0.0.1', '2018-08-05 17:32:08.186', 'JIDUSERVfszUmquMh0Ae2laygDp', '127.0.0.1'),
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSERDMZOs8UHqjsw9DGbB31z', NULL, 'APPROVED', '2018-08-05 17:32:25.052', 'JIDUSERDMZOs8UHqjsw9DGbB31z', '127.0.0.1', '2018-08-05 17:32:25.052', 'JIDUSERDMZOs8UHqjsw9DGbB31z', '127.0.0.1'),
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSER7uMucIkm1exJTu7sJvxR', NULL, 'APPROVED', '2018-08-05 17:32:38.079', 'JIDUSER7uMucIkm1exJTu7sJvxR', '127.0.0.1', '2018-08-05 17:32:38.079', 'JIDUSER7uMucIkm1exJTu7sJvxR', '127.0.0.1');
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSERVfszUmquMh0Ae2laygDp', NULL, 'APPROVED', '2018-08-05 17:32:08.186', 'JIDUSERVfszUmquMh0Ae2laygDp', NULL, '2018-08-05 17:32:08.186', 'JIDUSERVfszUmquMh0Ae2laygDp', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSERDMZOs8UHqjsw9DGbB31z', NULL, 'APPROVED', '2018-08-05 17:32:25.052', 'JIDUSERDMZOs8UHqjsw9DGbB31z', NULL, '2018-08-05 17:32:25.052', 'JIDUSERDMZOs8UHqjsw9DGbB31z', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSER7uMucIkm1exJTu7sJvxR', NULL, 'APPROVED', '2018-08-05 17:32:38.079', 'JIDUSER7uMucIkm1exJTu7sJvxR', NULL, '2018-08-05 17:32:38.079', 'JIDUSER7uMucIkm1exJTu7sJvxR', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSER5QiWnVJWzKdjtWtgEfN3', NULL, 'APPROVED', '2018-08-20 15:15:21.928', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:15:21.928', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSERXCcE36qmrr8Tm46k3hSV', NULL, 'APPROVED', '2018-08-20 15:15:26.677', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:15:26.677', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDUSERP1gi91MNc1XbXLbPx3dq', NULL, 'APPROVED', '2018-08-20 15:15:28.708', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:15:28.708', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDUSER7uMucIkm1exJTu7sJvxR', NULL, 'APPROVED', '2018-08-20 15:17:43.929', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:17:43.929', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDUSERDMZOs8UHqjsw9DGbB31z', NULL, 'APPROVED', '2018-08-20 15:17:47.499', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:17:47.499', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDUSERVfszUmquMh0Ae2laygDp', NULL, 'APPROVED', '2018-08-20 15:17:50.373', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:17:50.373', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDUSER5QiWnVJWzKdjtWtgEfN3', NULL, 'APPROVED', '2018-08-20 15:17:53.406', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:17:53.406', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDUSERXCcE36qmrr8Tm46k3hSV', NULL, 'APPROVED', '2018-08-20 15:17:56.161', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:17:56.161', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDUSERP1gi91MNc1XbXLbPx3dq', NULL, 'APPROVED', '2018-08-20 15:17:59.518', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:17:59.518', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
 
 --
 -- Dumping data for table `uriel_contest_module`
 --
 
 INSERT INTO `uriel_contest_module` (`contestJid`, `name`, `config`, `enabled`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'CLARIFICATION', '{}', b'1', '2018-08-05 19:59:23.330', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:23.330', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'SCOREBOARD', '{\"isIncognitoScoreboard\":false}', b'1', '2018-08-05 19:59:25.602', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:25.602', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'SUPERVISOR', '{}', b'1', '2018-08-05 19:59:28.084', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:28.084', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONTYsWkzwPy8wCF3maJfxqN', 'REGISTRATION', '{\"registerStartTime\":1533473972812,\"manualApproval\":false,\"registerDuration\":432000000,\"maxRegistrants\":0}', b'1', '2018-08-05 19:59:32.812', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:32.812', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONT89YMatMq7GqaPQCyyUHU', 'CLARIFICATION', '{}', b'1', '2018-08-05 19:59:50.281', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:50.281', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONT89YMatMq7GqaPQCyyUHU', 'SUPERVISOR', '{}', b'1', '2018-08-05 19:59:52.148', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:52.148', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONT89YMatMq7GqaPQCyyUHU', 'SCOREBOARD', '{\"isIncognitoScoreboard\":false}', b'1', '2018-08-05 19:59:54.004', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:54.004', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONT89YMatMq7GqaPQCyyUHU', 'REGISTRATION', '{\"registerStartTime\":1533473997175,\"manualApproval\":false,\"registerDuration\":432000000,\"maxRegistrants\":0}', b'1', '2018-08-05 19:59:57.175', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 19:59:57.175', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1');
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'CLARIFICATION', '{}', b'1', '2018-08-05 19:59:23.330', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:23.330', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'SCOREBOARD', '{\"isIncognitoScoreboard\":false}', b'1', '2018-08-05 19:59:25.602', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:25.602', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'SUPERVISOR', '{}', b'1', '2018-08-05 19:59:28.084', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:28.084', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'REGISTRATION', '{\"registerStartTime\":1533473972812,\"manualApproval\":false,\"registerDuration\":432000000,\"maxRegistrants\":0}', b'1', '2018-08-05 19:59:32.812', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:32.812', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'CLARIFICATION', '{}', b'1', '2018-08-05 19:59:50.281', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:50.281', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'SUPERVISOR', '{}', b'1', '2018-08-05 19:59:52.148', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:52.148', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'SCOREBOARD', '{\"isIncognitoScoreboard\":false}', b'1', '2018-08-05 19:59:54.004', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:54.004', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'REGISTRATION', '{\"registerStartTime\":1533473997175,\"manualApproval\":false,\"registerDuration\":432000000,\"maxRegistrants\":0}', b'1', '2018-08-05 19:59:57.175', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 19:59:57.175', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
+
+--
+-- Dumping data for table `uriel_contest_problem`
+--
+
+INSERT INTO `uriel_contest_problem` (`contestJid`, `problemJid`, `alias`, `submissionsLimit`, `status`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDPROGSxtSvuGAW4IKreFasSY0', 'A', 0, 'OPEN', '2018-08-20 00:00:00.000', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 00:00:00.000', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONTYsWkzwPy8wCF3maJfxqN', 'JIDPROGw21Ya31fwCuEnNJLlVwO', 'B', 0, 'OPEN', '2018-08-20 00:00:00.000', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 00:00:00.000', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDPROGSxtSvuGAW4IKreFasSY0', 'A', 0, 'OPEN', '2018-08-20 15:23:43.499', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:23:43.499', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', 'JIDPROGw21Ya31fwCuEnNJLlVwO', 'B', 0, 'OPEN', '2018-08-20 15:24:55.672', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-20 15:24:55.672', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
+
+--
+-- Dumping data for table `uriel_contest_scoreboard`
+--
+
+INSERT INTO `uriel_contest_scoreboard` (`scoreboard`, `type`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`, `contestJid`) VALUES
+('{\"state\":{\"problemJids\":[\"JIDPROGSxtSvuGAW4IKreFasSY0\",\"JIDPROGme6wb9DCQPCKJBEPO4qR\"],\"problemAliases\":[\"A\",\"B\"],\"contestantJids\":[\"JIDUSER7uMucIkm1exJTu7sJvxR\",\"JIDUSERDMZOs8UHqjsw9DGbB31z\",\"JIDUSERVfszUmquMh0Ae2laygDp\",\"JIDUSER5QiWnVJWzKdjtWtgEfN3\",\"JIDUSERXCcE36qmrr8Tm46k3hSV\",\"JIDUSERP1gi91MNc1XbXLbPx3dq\"]},\"content\":{\"entries\":[{\"rank\":1,\"contestantJid\":\"JIDUSERP1gi91MNc1XbXLbPx3dq\",\"totalAccepted\":2,\"totalPenalties\":30,\"lastAcceptedPenalty\":20,\"attemptsList\":[1,1],\"penaltyList\":[10,20],\"problemStateList\":[2,1]},{\"rank\":2,\"contestantJid\":\"JIDUSER7uMucIkm1exJTu7sJvxR\",\"totalAccepted\":2,\"totalPenalties\":31,\"lastAcceptedPenalty\":15,\"attemptsList\":[1,1],\"penaltyList\":[16,15],\"problemStateList\":[1,2]},{\"rank\":3,\"contestantJid\":\"JIDUSERVfszUmquMh0Ae2laygDp\",\"totalAccepted\":1,\"totalPenalties\":50,\"lastAcceptedPenalty\":30,\"attemptsList\":[2,1],\"penaltyList\":[30,15],\"problemStateList\":[1,0]},{\"rank\":4,\"contestantJid\":\"JIDUSERXCcE36qmrr8Tm46k3hSV\",\"totalAccepted\":0,\"totalPenalties\":0,\"lastAcceptedPenalty\":0,\"attemptsList\":[2,0],\"penaltyList\":[0,0],\"problemStateList\":[0,0]},{\"rank\":4,\"contestantJid\":\"JIDUSER5QiWnVJWzKdjtWtgEfN3\",\"totalAccepted\":0,\"totalPenalties\":0,\"lastAcceptedPenalty\":0,\"attemptsList\":[0,0],\"penaltyList\":[0,0],\"problemStateList\":[0,0]},{\"rank\":4,\"contestantJid\":\"JIDUSERDMZOs8UHqjsw9DGbB31z\",\"totalAccepted\":0,\"totalPenalties\":0,\"lastAcceptedPenalty\":0,\"attemptsList\":[0,0],\"penaltyList\":[0,0],\"problemStateList\":[0,0]}]}}', 'OFFICIAL', '2018-08-20 00:00:00.000', NULL, NULL, '2018-08-20 15:41:43.186', NULL, NULL, 'JIDCONTYsWkzwPy8wCF3maJfxqN'),
+('{\"state\":{\"problemJids\":[\"JIDPROGSxtSvuGAW4IKreFasSY0\",\"JIDPROGw21Ya31fwCuEnNJLlVwO\"],\"problemAliases\":[\"A\",\"B\"],\"contestantJids\":[\"JIDUSER7uMucIkm1exJTu7sJvxR\",\"JIDUSERDMZOs8UHqjsw9DGbB31z\",\"JIDUSERVfszUmquMh0Ae2laygDp\",\"JIDUSER5QiWnVJWzKdjtWtgEfN3\",\"JIDUSERXCcE36qmrr8Tm46k3hSV\",\"JIDUSERP1gi91MNc1XbXLbPx3dq\"]},\"content\":{\"entries\":[{\"rank\":1,\"contestantJid\":\"JIDUSERP1gi91MNc1XbXLbPx3dq\",\"scores\":[100,100],\"totalScores\":200,\"lastAffectingPenalty\":0},{\"rank\":2,\"contestantJid\":\"JIDUSER7uMucIkm1exJTu7sJvxR\",\"scores\":[100,50],\"totalScores\":150,\"lastAffectingPenalty\":0},{\"rank\":3,\"contestantJid\":\"JIDUSERVfszUmquMh0Ae2laygDp\",\"scores\":[75,0],\"totalScores\":75,\"lastAffectingPenalty\":0},{\"rank\":4,\"contestantJid\":\"JIDUSERXCcE36qmrr8Tm46k3hSV\",\"scores\":[0,null],\"totalScores\":0,\"lastAffectingPenalty\":0},{\"rank\":4,\"contestantJid\":\"JIDUSER5QiWnVJWzKdjtWtgEfN3\",\"scores\":[null,null],\"totalScores\":0,\"lastAffectingPenalty\":0},{\"rank\":4,\"contestantJid\":\"JIDUSERDMZOs8UHqjsw9DGbB31z\",\"scores\":[null,null],\"totalScores\":0,\"lastAffectingPenalty\":0}]}}', 'OFFICIAL', '2018-08-20 15:33:27.529', NULL, NULL, '2018-08-20 15:35:43.211', NULL, NULL, 'JIDCONT89YMatMq7GqaPQCyyUHU');
 
 --
 -- Dumping data for table `uriel_contest_style`
 --
 
 INSERT INTO `uriel_contest_style` (`contestJid`, `config`, `createdAt`, `createdBy`, `createdIp`, `updatedAt`, `updatedBy`, `updatedIp`) VALUES
-('JIDCONTYsWkzwPy8wCF3maJfxqN', '{\"wrongSubmissionPenalty\":20,\"languageRestriction\":{\"allowedLanguageNames\":[]}}', '2018-08-05 17:30:35.327', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 17:30:35.327', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1'),
-('JIDCONT89YMatMq7GqaPQCyyUHU', '{\"usingLastAffectingPenalty\":false,\"languageRestriction\":{\"allowedLanguageNames\":[]}}', '2018-08-05 18:11:40.037', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1', '2018-08-05 18:11:40.037', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', '127.0.0.1');
+('JIDCONTYsWkzwPy8wCF3maJfxqN', '{\"wrongSubmissionPenalty\":20,\"languageRestriction\":{\"allowedLanguageNames\":[]}}', '2018-08-05 17:30:35.327', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 17:30:35.327', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL),
+('JIDCONT89YMatMq7GqaPQCyyUHU', '{\"usingLastAffectingPenalty\":false,\"languageRestriction\":{\"allowedLanguageNames\":[]}}', '2018-08-05 18:11:40.037', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL, '2018-08-05 18:11:40.037', 'JIDUSERGBsbuD2EJ9jYMlGclISJ', NULL);
 
 --
 -- Dumping data for table `uriel_role_admin`
 --
 
 INSERT INTO `uriel_role_admin` (`userJid`, `createdAt`, `createdBy`, `createdIp`) VALUES
-('JIDCONT89YMatMq7GqaPQCyyUHU', '2018-08-05 00:00:00.000', NULL, NULL);
+('JIDUSERGBsbuD2EJ9jYMlGclISJ', '2018-08-05 00:00:00.000', NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
