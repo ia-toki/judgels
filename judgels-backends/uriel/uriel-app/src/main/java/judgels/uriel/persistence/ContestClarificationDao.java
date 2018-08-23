f78198e189428325106310583f2f6d91f2ccfdf4
Febriananda Wida Pramudita <p.phebri@yahoo.com>
2018-08-23 13:17:52 +0700

    fix issue spv should see all clarif, add disable module, fix test

--- judgels-backends/uriel/uriel-app/src/main/java/judgels/uriel/persistence/ContestClarificationDao.java
+++ judgels-backends/uriel/uriel-app/src/main/java/judgels/uriel/persistence/ContestClarificationDao.java
@@ -5,5 +5,6 @@ import judgels.persistence.JudgelsDao;
 
 public interface ContestClarificationDao extends JudgelsDao<ContestClarificationModel> {
     List<ContestClarificationModel> selectAllByContestJidAndUserJid(String contestJid, String userJid);
+    List<ContestClarificationModel> selectAllByContestJid(String contestJid);
     long selectCountAnsweredByContestJidAndUserJid(String contestJid, String userJid);
 }
