ae7ea831e93022bef307f0e2781d4d4e0ecb71c5
Ashar Fuadi <fushar@gmail.com>
2018-08-28 18:39:59 +0700

    Release 2.5.0

--- judgels-frontends/raphael/src/components/Footer/Footer.tsx
+++ judgels-frontends/raphael/src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => (
     <div className="footer__text">
       <div className="footer__left">&copy; 2018 Ikatan Alumni TOKI</div>
       <div className="footer__right">
-        Powered by <a href="https://github.com/ia-toki/judgels">Judgels</a> 2.4.1
+        Powered by <a href="https://github.com/ia-toki/judgels">Judgels</a> 2.5.0
       </div>
     </div>
   </div>
