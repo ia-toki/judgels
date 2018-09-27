0331354e7c52a997d17a18e5f25e4f86b2b87060
Ashar Fuadi <fushar@gmail.com>
2018-09-27 08:05:51 +0700

    Release 2.10.0

--- judgels-backends/judgels-commons/judgels-service-core/src/main/java/judgels/service/JudgelsVersion.java
+++ judgels-backends/judgels-commons/judgels-service-core/src/main/java/judgels/service/JudgelsVersion.java
@@ -1,7 +1,7 @@
 package judgels.service;
 
 public final class JudgelsVersion {
-    public static final String INSTANCE = "2.9.0";
+    public static final String INSTANCE = "2.10.0";
 
     private JudgelsVersion() {}
 }
