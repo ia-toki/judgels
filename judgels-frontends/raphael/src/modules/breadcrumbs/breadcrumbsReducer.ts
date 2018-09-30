dc2c22d7b0a043574ce7210aca5d14ab7eabddab
Kevin Luvian <kluvian001@e.ntu.edu.sg>
2018-09-30 23:50:15 +0800

    move remove trailing slash to reducer

--- judgels-frontends/raphael/src/modules/breadcrumbs/breadcrumbsReducer.ts
+++ judgels-frontends/raphael/src/modules/breadcrumbs/breadcrumbsReducer.ts
@@ -17,14 +17,18 @@ export const PopBreadcrumb = TypedAction.define('breadcrumbs/POP')<{
   link: string;
 }>();
 
+const cleanLink = (link) => {
+  return link.replace(/\/+$/, '')
+};
+
 const createBreadcrumbsReducer = () => {
   const builder = TypedReducer.builder<BreadcrumbsState>();
 
-  builder.withHandler(PushBreadcrumb.TYPE, (state, payload) => ({
-    values: [...state.values, payload],
+  builder.withHandler(PushBreadcrumb.TYPE, (state, { link, ...rest }) => ({
+    values: [...state.values, { link: cleanLink(link), ...rest }],
   }));
   builder.withHandler(PopBreadcrumb.TYPE, (state, payload) => ({
-    values: state.values.filter(b => b.link !== payload.link),
+    values: state.values.filter(b => b.link !== cleanLink(payload.link)),
   }));
   builder.withDefaultHandler(state => (state !== undefined ? state : INITIAL_STATE));
 
