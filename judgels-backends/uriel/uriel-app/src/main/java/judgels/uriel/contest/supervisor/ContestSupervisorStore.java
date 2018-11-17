94bc54e4711677a4ba5e69bb02666ec96e517d3a
Ashar Fuadi <fushar@gmail.com>
2018-11-17 20:19:17 +0700

    Use cache in supervisor store

--- judgels-backends/uriel/uriel-app/src/main/java/judgels/uriel/contest/supervisor/ContestSupervisorStore.java
+++ judgels-backends/uriel/uriel-app/src/main/java/judgels/uriel/contest/supervisor/ContestSupervisorStore.java
@@ -1,7 +1,12 @@
 package judgels.uriel.contest.supervisor;
 
+import static judgels.uriel.UrielCacheUtils.SEPARATOR;
+import static judgels.uriel.UrielCacheUtils.getShortDuration;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import java.io.IOException;
 import java.util.Optional;
 import javax.inject.Inject;
@@ -15,30 +20,47 @@ public class ContestSupervisorStore {
     private final ContestSupervisorDao supervisorDao;
     private final ObjectMapper mapper;
 
+    private final Cache<String, ContestSupervisor> supervisorCache;
+
     @Inject
     public ContestSupervisorStore(ContestSupervisorDao supervisorDao, ObjectMapper mapper) {
         this.supervisorDao = supervisorDao;
         this.mapper = mapper;
+
+        this.supervisorCache = Caffeine.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(getShortDuration())
+                .build();
     }
 
     public Optional<ContestSupervisor> getSupervisor(String contestJid, String userJid) {
+        return Optional.ofNullable(supervisorCache.get(
+                contestJid + SEPARATOR + userJid,
+                $ -> getSupervisorUncached(contestJid, userJid)));
+    }
+
+    private ContestSupervisor getSupervisorUncached(String contestJid, String userJid) {
         return supervisorDao.selectByContestJidAndUserJid(contestJid, userJid)
-                .map(this::fromModel);
+                .map(this::fromModel)
+                .orElse(null);
     }
 
     public ContestSupervisor upsertSupervisor(String contestJid, ContestSupervisorData data) {
         Optional<ContestSupervisorModel> maybeModel =
                 supervisorDao.selectByContestJidAndUserJid(contestJid, data.getUserJid());
 
+        ContestSupervisor supervisor;
         if (maybeModel.isPresent()) {
             ContestSupervisorModel model = maybeModel.get();
             toModel(contestJid, data, model);
-            return fromModel(supervisorDao.update(model));
+            supervisor = fromModel(supervisorDao.update(model));
         } else {
             ContestSupervisorModel model = new ContestSupervisorModel();
             toModel(contestJid, data, model);
-            return fromModel(supervisorDao.insert(model));
+            supervisor = fromModel(supervisorDao.insert(model));
         }
+        supervisorCache.invalidate(contestJid + SEPARATOR + data.getUserJid());
+        return supervisor;
     }
 
     private ContestSupervisor fromModel(ContestSupervisorModel model) {
