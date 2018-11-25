71897a6d22f48fdc9b1cac42dc733c0d581874bc
Ashar Fuadi <fushar@gmail.com>
2018-11-25 11:53:40 +0700

    Release 2.18.0

--- judgels-backends/judgels-commons/judgels-service-core/src/main/java/judgels/service/JudgelsVersion.java
+++ judgels-backends/judgels-commons/judgels-service-core/src/main/java/judgels/service/JudgelsVersion.java
@@ -1,7 +1,7 @@
 package judgels.service;
 
 public final class JudgelsVersion {
-    public static final String INSTANCE = "2.17.0";
+    public static final String INSTANCE = "2.18.0";
 
     private JudgelsVersion() {}
 }
--- judgels-frontends/raphael/src/version.ts
+++ judgels-frontends/raphael/src/version.ts
@@ -1,1 +1,1 @@
-export const JUDGELS_VERSION = '2.17.0';
+export const JUDGELS_VERSION = '2.18.0';
