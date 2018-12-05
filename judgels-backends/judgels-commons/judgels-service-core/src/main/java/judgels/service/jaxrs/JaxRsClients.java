e7ae5c2982fa4f8700175ed797c5f1fcfde65258
Ashar Fuadi <fushar@gmail.com>
2018-12-05 22:23:20 +0700

    [refactor] Use reflection to inject ObjectMapper to JaxRsClient

--- judgels-backends/judgels-commons/judgels-service-core/src/main/java/judgels/service/jaxrs/JaxRsClients.java
+++ judgels-backends/judgels-commons/judgels-service-core/src/main/java/judgels/service/jaxrs/JaxRsClients.java
@@ -5,32 +5,13 @@ import com.google.common.collect.ImmutableList;
 import com.palantir.conjure.java.api.config.service.UserAgent;
 import com.palantir.conjure.java.client.config.ClientConfiguration;
 import com.palantir.conjure.java.client.config.ClientConfigurations;
-import com.palantir.conjure.java.client.jaxrs.feignimpl.Java8OptionalAwareContract;
-import com.palantir.conjure.java.client.jaxrs.feignimpl.PathTemplateHeaderEnrichmentContract;
-import com.palantir.conjure.java.client.jaxrs.feignimpl.PathTemplateHeaderRewriter;
-import com.palantir.conjure.java.client.jaxrs.feignimpl.SlashEncodingContract;
+import com.palantir.conjure.java.client.jaxrs.FeignJaxRsClientBuilder;
 import com.palantir.conjure.java.okhttp.HostEventsSink;
 import com.palantir.conjure.java.okhttp.HostMetricsRegistry;
-import com.palantir.conjure.java.okhttp.OkHttpClients;
 import com.palantir.conjure.java.serialization.ObjectMappers;
-import feign.Contract;
-import feign.EmptyContainerDecoder;
-import feign.Feign;
-import feign.InputStreamDelegateDecoder;
-import feign.InputStreamDelegateEncoder;
-import feign.Java8OptionalAwareDecoder;
-import feign.Logger;
-import feign.NeverReturnNullDecoder;
-import feign.Request;
-import feign.Retryer;
-import feign.TextDelegateDecoder;
-import feign.TextDelegateEncoder;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
-import feign.jaxrs.JAXRSContract;
-import feign.okhttp.OkHttpClient;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -73,52 +54,29 @@ public class JaxRsClients {
 
     private JaxRsClients() {}
 
-    // Adapted from com.palantir.conjure.java.client.jaxrs.AbstractFeignJaxRsClientBuilder.
-    // This exists because we want to modify the object mapper via JudgelsObjectMappers.
     public static <T> T create(Class<T> serviceClass, String uri, UserAgent userAgent) {
         List<String> uris = ImmutableList.of(uri);
         ClientConfiguration clientConfig = ClientConfigurations.of(uris, defaultSslSocketFactory, defaultTrustManager);
         ObjectMapper objectMapper = JudgelsObjectMappers.configure(ObjectMappers.newClientObjectMapper());
         HostEventsSink eventsSink = new HostMetricsRegistry();
 
-        return Feign.builder()
-                .contract(createContract())
-                .encoder(createEncoder(objectMapper))
-                .decoder(createDecoder(objectMapper))
-                .requestInterceptor(PathTemplateHeaderRewriter.INSTANCE)
-                .client(new OkHttpClient(OkHttpClients.create(clientConfig, userAgent, eventsSink, serviceClass)))
-                .options(createRequestOptions(clientConfig))
-                .logLevel(Logger.Level.NONE)  // we use OkHttp interceptors for logging. (note that NONE is the default)
-                .retryer(new Retryer.Default(0, 0, 1))  // use OkHttp retry mechanism only
-                .target(serviceClass, uri);
-    }
+        // This is an unfortunate hack to inject a custom object mapper to the JAX-RS client.
+        try {
+            Constructor<FeignJaxRsClientBuilder> ctor =
+                    FeignJaxRsClientBuilder.class.getDeclaredConstructor(ClientConfiguration.class);
+            ctor.setAccessible(true);
+            FeignJaxRsClientBuilder builder = ctor.newInstance(clientConfig);
 
-    private static Contract createContract() {
-        return new PathTemplateHeaderEnrichmentContract(
-                new SlashEncodingContract(
-                        new Java8OptionalAwareContract(
-                                new JAXRSContract())));
-    }
+            Field builderMapper = FeignJaxRsClientBuilder.class.getDeclaredField("JSON_OBJECT_MAPPER");
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(builderMapper, builderMapper.getModifiers() & ~Modifier.FINAL);
+            builderMapper.setAccessible(true);
+            builderMapper.set(builder, objectMapper);
 
-    private static Request.Options createRequestOptions(ClientConfiguration clientConfig) {
-        return new Request.Options(
-                Math.toIntExact(clientConfig.connectTimeout().toMillis()),
-                Math.toIntExact(clientConfig.readTimeout().toMillis()));
-    }
-
-    private static Encoder createEncoder(ObjectMapper objectMapper) {
-        return new InputStreamDelegateEncoder(
-                new TextDelegateEncoder(
-                        new JacksonEncoder(objectMapper)));
-    }
-
-    private static Decoder createDecoder(ObjectMapper objectMapper) {
-        return new NeverReturnNullDecoder(
-                new Java8OptionalAwareDecoder(
-                        new EmptyContainerDecoder(
-                                objectMapper,
-                                new InputStreamDelegateDecoder(
-                                        new TextDelegateDecoder(
-                                                new JacksonDecoder(objectMapper))))));
+            return ((FeignJaxRsClientBuilder) (builder.hostEventsSink(eventsSink))).build(serviceClass, userAgent);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
