ee95d5049a7c97b9b713f673ae2e036e0653edb1
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2018-12-21 11:30:37 +0700

    Remove wild import in ContestRoleCheckerIntegrationTests

--- judgels-backends/uriel/uriel-app/src/integTest/java/judgels/uriel/contest/ContestRoleCheckerIntegrationTests.java
+++ judgels-backends/uriel/uriel-app/src/integTest/java/judgels/uriel/contest/ContestRoleCheckerIntegrationTests.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 import judgels.uriel.api.contest.module.VirtualModuleConfig;
 import judgels.uriel.api.contest.role.ContestRole;
 import judgels.uriel.contest.role.AbstractRoleCheckerIntegrationTests;
-import net.bytebuddy.implementation.bind.annotation.Super;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
