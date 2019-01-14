bca2eb274c3b30e976fc4eb574571fd271ff61df
Ashar Fuadi <fushar@gmail.com>
2019-01-14 06:31:39 -0800

    [cleanup] Remove unused legacy user endpoints

--- judgels-backends/jophiel/jophiel-app/src/main/java/judgels/jophiel/legacy/user/LegacyUserResource.java
+++ judgels-backends/jophiel/jophiel-app/src/main/java/judgels/jophiel/legacy/user/LegacyUserResource.java
@@ -1,18 +1,12 @@
 package judgels.jophiel.legacy.user;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static judgels.service.ServiceUtils.checkFound;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import io.dropwizard.hibernate.UnitOfWork;
-import java.net.URI;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import judgels.jophiel.api.user.User;
@@ -30,14 +24,6 @@ public class LegacyUserResource {
     }
 
     @GET
-    @Path("/username/{username}")
-    @Produces(APPLICATION_JSON)
-    @UnitOfWork(readOnly = true)
-    public User getUserByUsername(@PathParam("username") String username) {
-        return checkFound(userStore.getUserByUsername(username));
-    }
-
-    @GET
     @Path("/autocomplete")
     @UnitOfWork(readOnly = true)
     public Response autocompleteUsers(@QueryParam("term") String term, @QueryParam("callback") String callback)
@@ -52,12 +38,5 @@ public class LegacyUserResource {
         String resJson = mapper.writeValueAsString(res);
 
         return Response.ok(callback + "(" + resJson + ");", "application/javascript").build();
-    }
-
-    @GET
-    @Path("/{userJid}/profile")
-    @UnitOfWork(readOnly = true)
-    public Response getUserProfile(@PathParam("userJid") String userJid) {
-        return Response.temporaryRedirect(URI.create("/api/v2/users/" + userJid + "/info")).build();
     }
 }
