9b1ba65b7ffc5e0a7d593fefdcd8b3f20fd9cf95
Ashar Fuadi <fushar@gmail.com>
2019-01-16 22:03:33 -0800

    [refactor] Kill legacy findClientXXX() endpoint in Sandalphon pt 2

--- judgels-legacy/sandalphon/app/org/iatoki/judgels/sandalphon/controllers/api/object/v1/ClientLessonFindRequestV1.java
+++
@@ -1,8 +0,0 @@
-package org.iatoki.judgels.sandalphon.controllers.api.object.v1;
-
-public final class ClientLessonFindRequestV1 {
-
-    public String clientJid;
-    public String lessonJid;
-    public String lessonSecret;
-}
--- judgels-legacy/sandalphon/app/org/iatoki/judgels/sandalphon/controllers/api/object/v1/ClientProblemFindRequestV1.java
+++
@@ -1,8 +0,0 @@
-package org.iatoki.judgels.sandalphon.controllers.api.object.v1;
-
-public final class ClientProblemFindRequestV1 {
-
-    public String clientJid;
-    public String problemJid;
-    public String problemSecret;
-}
