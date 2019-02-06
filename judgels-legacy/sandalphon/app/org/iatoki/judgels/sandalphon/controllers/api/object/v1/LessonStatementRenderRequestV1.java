64635ef4c64d947fbceff8b63e966d1dcc616e91
Ashar Fuadi <fushar@gmail.com>
2019-02-06 08:19:21 +0700

    Fix Sandalphon not be able to render statements after migrating to Gradle

--- judgels-legacy/sandalphon/app/org/iatoki/judgels/sandalphon/controllers/api/object/v1/LessonStatementRenderRequestV1.java
+++ judgels-legacy/sandalphon/app/org/iatoki/judgels/sandalphon/controllers/api/object/v1/LessonStatementRenderRequestV1.java
@@ -4,8 +4,47 @@ public final class LessonStatementRenderRequestV1 {
 
     public String lessonJid;
     public String clientJid;
-    public long currentMillis;
     public int totpCode;
     public String statementLanguage;
     public String switchStatementLanguageUrl;
+
+    public String getLessonJid() {
+        return lessonJid;
+    }
+
+    public void setLessonJid(String lessonJid) {
+        this.lessonJid = lessonJid;
+    }
+
+    public String getClientJid() {
+        return clientJid;
+    }
+
+    public void setClientJid(String clientJid) {
+        this.clientJid = clientJid;
+    }
+
+    public int getTotpCode() {
+        return totpCode;
+    }
+
+    public void setTotpCode(int totpCode) {
+        this.totpCode = totpCode;
+    }
+
+    public String getStatementLanguage() {
+        return statementLanguage;
+    }
+
+    public void setStatementLanguage(String statementLanguage) {
+        this.statementLanguage = statementLanguage;
+    }
+
+    public String getSwitchStatementLanguageUrl() {
+        return switchStatementLanguageUrl;
+    }
+
+    public void setSwitchStatementLanguageUrl(String switchStatementLanguageUrl) {
+        this.switchStatementLanguageUrl = switchStatementLanguageUrl;
+    }
 }
--- judgels-legacy/sandalphon/app/org/iatoki/judgels/sandalphon/controllers/api/object/v1/ProgrammingProblemStatementRenderRequestV1.java
+++ judgels-legacy/sandalphon/app/org/iatoki/judgels/sandalphon/controllers/api/object/v1/ProgrammingProblemStatementRenderRequestV1.java
@@ -10,4 +10,68 @@ public final class ProgrammingProblemStatementRenderRequestV1 {
     public String postSubmitUrl;
     public String reasonNotAllowedToSubmit;
     public String allowedGradingLanguages;
+
+    public String getProblemJid() {
+        return problemJid;
+    }
+
+    public void setProblemJid(String problemJid) {
+        this.problemJid = problemJid;
+    }
+
+    public String getClientJid() {
+        return clientJid;
+    }
+
+    public void setClientJid(String clientJid) {
+        this.clientJid = clientJid;
+    }
+
+    public int getTotpCode() {
+        return totpCode;
+    }
+
+    public void setTotpCode(int totpCode) {
+        this.totpCode = totpCode;
+    }
+
+    public String getStatementLanguage() {
+        return statementLanguage;
+    }
+
+    public void setStatementLanguage(String statementLanguage) {
+        this.statementLanguage = statementLanguage;
+    }
+
+    public String getSwitchStatementLanguageUrl() {
+        return switchStatementLanguageUrl;
+    }
+
+    public void setSwitchStatementLanguageUrl(String switchStatementLanguageUrl) {
+        this.switchStatementLanguageUrl = switchStatementLanguageUrl;
+    }
+
+    public String getPostSubmitUrl() {
+        return postSubmitUrl;
+    }
+
+    public void setPostSubmitUrl(String postSubmitUrl) {
+        this.postSubmitUrl = postSubmitUrl;
+    }
+
+    public String getReasonNotAllowedToSubmit() {
+        return reasonNotAllowedToSubmit;
+    }
+
+    public void setReasonNotAllowedToSubmit(String reasonNotAllowedToSubmit) {
+        this.reasonNotAllowedToSubmit = reasonNotAllowedToSubmit;
+    }
+
+    public String getAllowedGradingLanguages() {
+        return allowedGradingLanguages;
+    }
+
+    public void setAllowedGradingLanguages(String allowedGradingLanguages) {
+        this.allowedGradingLanguages = allowedGradingLanguages;
+    }
 }
