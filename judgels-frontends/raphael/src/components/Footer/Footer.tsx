f780b114d06c9266151fd5f237fb1bc7c73a92eb
Ashar Fuadi <fushar@gmail.com>
2019-03-01 09:01:50 +0700

    Happy belated new year 2019

--- judgels-frontends/raphael/src/components/Footer/Footer.tsx
+++ judgels-frontends/raphael/src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => (
   <div className="footer">
     <hr />
     <div className="footer__text">
-      <div className="footer__left">&copy; 2018 Ikatan Alumni TOKI</div>
+      <div className="footer__left">&copy; 2019 Ikatan Alumni TOKI</div>
       <div className="footer__right">
         Powered by <a href="https://github.com/ia-toki/judgels">Judgels</a> {JUDGELS_VERSION}
       </div>
