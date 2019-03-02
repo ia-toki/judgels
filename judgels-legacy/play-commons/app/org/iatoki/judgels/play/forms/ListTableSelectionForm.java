1971e6d0843d17126d67b1168c811e570c4a90bd
Ashar Fuadi <fushar@gmail.com>
2019-03-02 21:41:27 +0700

    Fix legacy regrade form missing required getter and setter in form

--- judgels-legacy/play-commons/app/org/iatoki/judgels/play/forms/ListTableSelectionForm.java
+++ judgels-legacy/play-commons/app/org/iatoki/judgels/play/forms/ListTableSelectionForm.java
@@ -7,4 +7,20 @@ public class ListTableSelectionForm {
     public boolean selectAll;
 
     public List<String> selectJids;
+
+    public boolean getSelectAll() {
+        return selectAll;
+    }
+
+    public void setSelectAll(boolean selectAll) {
+        this.selectAll = selectAll;
+    }
+
+    public List<String> getSelectJids() {
+        return selectJids;
+    }
+
+    public void setSelectJids(List<String> selectJids) {
+        this.selectJids = selectJids;
+    }
 }
