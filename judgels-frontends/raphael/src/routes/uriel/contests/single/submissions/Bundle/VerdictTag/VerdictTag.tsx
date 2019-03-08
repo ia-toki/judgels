e5a28a1cb42c149f2997337f12bf369e070ba00f
Jauhar Arifin <jauhararifin10@gmail.com>
2019-03-08 11:43:56 +0700

    use consistent verdict tag

--- judgels-frontends/raphael/src/routes/uriel/contests/single/submissions/Bundle/VerdictTag/VerdictTag.tsx
+++ judgels-frontends/raphael/src/routes/uriel/contests/single/submissions/Bundle/VerdictTag/VerdictTag.tsx
@@ -1,38 +1,39 @@
 import * as React from 'react';
-import { Tag, Intent } from '@blueprintjs/core';
+import { Tag, Intent, Icon } from '@blueprintjs/core';
 import { Verdict } from 'modules/api/sandalphon/submissionBundle';
 
 export interface VerdictTagProps {
   verdict: Verdict;
 }
 
+const verdictIntentMap: { [verdict: string]: Intent } = {
+  [Verdict.ACCEPTED]: Intent.SUCCESS,
+  [Verdict.GRADING_NOT_NEEDED]: Intent.NONE,
+  [Verdict.INTERNAL_ERROR]: Intent.DANGER,
+  [Verdict.NO_ANSWER]: Intent.NONE,
+  [Verdict.OK]: Intent.SUCCESS,
+  [Verdict.PENDING]: Intent.NONE,
+  [Verdict.PENDING_MANUAL_GRADING]: Intent.NONE,
+  [Verdict.WRONG_ANSWER]: Intent.DANGER,
+};
+
+const verdictDisplayCode: { [verdict: string]: string | React.ReactNode } = {
+  [Verdict.ACCEPTED]: 'AC',
+  [Verdict.GRADING_NOT_NEEDED]: 'Not Needed',
+  [Verdict.INTERNAL_ERROR]: 'IE',
+  [Verdict.NO_ANSWER]: 'No Ans',
+  [Verdict.OK]: 'OK',
+  [Verdict.PENDING]: <Icon icon="time" />,
+  [Verdict.PENDING_MANUAL_GRADING]: 'MNL',
+  [Verdict.WRONG_ANSWER]: 'WA',
+};
+
 export const VerdictTag: React.FunctionComponent<VerdictTagProps> = ({ verdict }) => {
-  let intent: Intent = Intent.NONE;
-  let text = 'None';
-  if (verdict === Verdict.ACCEPTED) {
-    intent = Intent.SUCCESS;
-    text = 'AC';
-  } else if (verdict === Verdict.GRADING_NOT_NEEDED) {
-    intent = Intent.NONE;
-    text = 'Not Needed';
-  } else if (verdict === Verdict.INTERNAL_ERROR) {
-    intent = Intent.DANGER;
-    text = 'IE';
-  } else if (verdict === Verdict.NO_ANSWER) {
-    intent = Intent.NONE;
-    text = 'No Ans';
-  } else if (verdict === Verdict.OK) {
-    intent = Intent.SUCCESS;
-    text = 'OK';
-  } else if (verdict === Verdict.PENDING) {
-    intent = Intent.NONE;
-    text = 'Pending';
-  } else if (verdict === Verdict.PENDING_MANUAL_GRADING) {
-    intent = Intent.NONE;
-    text = 'Pending Manual';
-  } else if (verdict === Verdict.WRONG_ANSWER) {
-    intent = Intent.DANGER;
-    text = 'WA';
-  }
-  return <Tag intent={intent}>{text}</Tag>;
+  const intent = verdictIntentMap[verdict] || Intent.NONE;
+  const displayCode = verdictDisplayCode[verdict] || verdict;
+  return (
+    <Tag round intent={intent}>
+      {displayCode}
+    </Tag>
+  );
 };
