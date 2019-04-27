f9f50e40d058f427ec0bc7942653673812d53064
Jauhar Arifin <jauhararifin10@gmail.com>
2019-04-27 13:55:30 +0700

    lazy loading tinymce (#197)

--- judgels-frontends/raphael/src/components/forms/FormRichTextArea/FormRichTextArea.tsx
+++ judgels-frontends/raphael/src/components/forms/FormRichTextArea/FormRichTextArea.tsx
@@ -1,55 +1,28 @@
 import { FormGroup } from '@blueprintjs/core';
 import * as React from 'react';
 
+import * as Loadable from 'react-loadable';
+import { LoadingState } from 'components/LoadingState/LoadingState';
+
 import { getIntent } from '../meta';
 import { FormInputProps } from '../props';
 import { FormInputValidation } from '../FormInputValidation/FormInputValidation';
 
-import tinymce from 'tinymce/tinymce';
-import 'tinymce/themes/modern/theme';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/image';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/lists';
-import 'tinymce/plugins/paste';
-import 'tinymce/plugins/table';
+const LoadableTinyMCETextArea = Loadable({
+  loader: () => import('./TinyMCETextArea'),
+  loading: () => <LoadingState large />,
+});
 
 export interface FormRichTextAreaProps extends FormInputProps {
   rows: number;
 }
 
 export class FormRichTextArea extends React.PureComponent<FormRichTextAreaProps> {
-  componentDidMount() {
-    if (tinymce) {
-      tinymce.init({
-        selector: '.tinymce',
-        skin_url: '/skins/lightgray',
-        content_css: '/skins/raphael/content.css',
-        branding: false,
-        menubar: false,
-        plugins: 'code image link lists table',
-        toolbar:
-          'bold italic underline strikethrough subscript superscript blockquote removeformat | ' +
-          'bullist numlist | outdent indent | ' +
-          'alignleft aligncenter alignright alignjustify | ' +
-          'image link table | ' +
-          'styleselect formatselect | ' +
-          'code',
-        setup: editor => {
-          editor.on('change', () => {
-            this.props.input.onChange(editor.getContent());
-          });
-        },
-      });
-    }
-  }
-
   render() {
     const { rows, input, label, meta } = this.props;
-
     return (
       <FormGroup labelFor={input.name} label={label} intent={getIntent(meta)}>
-        <textarea {...input} className="tinymce" rows={rows} />
+        <LoadableTinyMCETextArea {...input} rows={rows} onChange={input.onChange} />
         <FormInputValidation meta={meta} />
       </FormGroup>
     );
