da8e89264c4624370476c072612b53fda1df0bf4
Ashar Fuadi <fushar@gmail.com>
2019-06-23 13:56:28 +0700

    Fix grading engine names mapping

--- judgels-backends/gabriel/gabriel-engines/src/main/java/judgels/gabriel/engines/GradingEngineRegistry.java
+++ judgels-backends/gabriel/gabriel-engines/src/main/java/judgels/gabriel/engines/GradingEngineRegistry.java
@@ -16,9 +16,9 @@ import judgels.gabriel.engines.outputonly.OutputOnlyWithSubtasksGradingEngine;
 
 public class GradingEngineRegistry {
     private static final GradingEngineRegistry INSTANCE = new GradingEngineRegistry();
-    private static final Class<? extends GradingEngine> DEFAULT_GRADING_ENGINE = BatchGradingEngine.class;
+    private static final Class<? extends GradingEngine> DEFAULT_CLASS = BatchGradingEngine.class;
 
-    private static final List<Class<? extends GradingEngine>> ENGINES = ImmutableList.of(
+    private static final List<Class<? extends GradingEngine>> CLASSES = ImmutableList.of(
             BatchGradingEngine.class,
             BatchWithSubtasksGradingEngine.class,
             InteractiveGradingEngine.class,
@@ -28,14 +28,14 @@ public class GradingEngineRegistry {
             FunctionalGradingEngine.class,
             FunctionalWithSubtasksGradingEngine.class);
 
-    private static final Map<String, Class<? extends GradingEngine>> ENGINES_BY_SIMPLE_NAME = ENGINES.stream().collect(
+    private static final Map<String, Class<? extends GradingEngine>> CLASSES_BY_SIMPLE_NAME = CLASSES.stream().collect(
             LinkedHashMap::new,
-            (map, engine) -> map.put(getSimpleName(engine), engine),
+            (map, clazz) -> map.put(getSimpleName(clazz), clazz),
             Map::putAll);
 
-    private static final Map<String, String> ENGINE_NAMES_BY_SIMPLE_NAME = ENGINES.stream().collect(
+    private static final Map<String, String> ENGINE_NAMES_BY_SIMPLE_NAME = CLASSES.stream().collect(
             LinkedHashMap::new,
-            (map, engine) -> map.put(getSimpleName(engine), engine.getName()),
+            (map, clazz) -> map.put(getSimpleName(clazz), newInstance(clazz).getName()),
             Map::putAll);
 
     private GradingEngineRegistry() {}
@@ -45,19 +45,15 @@ public class GradingEngineRegistry {
     }
 
     public GradingEngine get(String simpleName) {
-        Class<? extends GradingEngine> engine = ENGINES_BY_SIMPLE_NAME.get(simpleName);
-        if (engine == null) {
+        Class<? extends GradingEngine> clazz = CLASSES_BY_SIMPLE_NAME.get(simpleName);
+        if (clazz == null) {
             throw new IllegalArgumentException("Grading engine " + simpleName + " not found");
         }
-        try {
-            return engine.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        return newInstance(clazz);
     }
 
     public String getDefault() {
-        return getSimpleName(DEFAULT_GRADING_ENGINE);
+        return getSimpleName(DEFAULT_CLASS);
     }
 
     public Map<String, String> getNamesMap() {
@@ -67,5 +63,13 @@ public class GradingEngineRegistry {
     private static String getSimpleName(Class<? extends GradingEngine> engine) {
         String name = engine.getSimpleName();
         return name.substring(0, name.length() - "GradingEngine".length());
+    }
+
+    private static GradingEngine newInstance(Class<? extends GradingEngine> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
