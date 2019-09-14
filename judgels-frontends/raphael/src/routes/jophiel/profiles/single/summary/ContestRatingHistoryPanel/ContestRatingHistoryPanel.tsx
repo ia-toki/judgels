a9ec1c86f3826d4d1fa2c274d348ee5d2138c126
Ashar Fuadi <fushar@gmail.com>
2019-09-14 20:58:07 +0700

    Temporarily remove contest rating history panel

--- judgels-frontends/raphael/src/routes/jophiel/profiles/single/summary/ContestRatingHistoryPanel/ContestRatingHistoryPanel.tsx
+++ judgels-frontends/raphael/src/routes/jophiel/profiles/single/summary/ContestRatingHistoryPanel/ContestRatingHistoryPanel.tsx
@@ -1,9 +1,5 @@
-import { HTMLTable } from '@blueprintjs/core';
 import * as React from 'react';
 
-import { Card } from '../../../../../../components/Card/Card';
-import { ContestLink } from '../../../../../../components/ContestLink/ContestLink';
-import { getRatingClass } from '../../../../../../modules/api/jophiel/userRating';
 import { ContestRatingHistoryResponse } from '../../../../../../modules/api/uriel/contestRating';
 
 export interface ContestRatingHistoryPanelProps {
@@ -12,32 +8,6 @@ export interface ContestRatingHistoryPanelProps {
 
 export class ContestRatingHistoryPanel extends React.PureComponent<ContestRatingHistoryPanelProps> {
   render() {
-    return (
-      <Card title="Contest rating history" className="profile-summary-card">
-        <HTMLTable striped condensed>
-          <thead>
-            <tr>
-              <th>Contest</th>
-              <th>New rating</th>
-            </tr>
-          </thead>
-          <tbody>{this.props.history.data.map(this.renderRow)}</tbody>
-        </HTMLTable>
-      </Card>
-    );
+    return null;
   }
-
-  private renderRow = (event, idx) => {
-    const contest = this.props.history.contestsMap[event.contestJid];
-    return (
-      <tr>
-        <td>
-          <ContestLink contest={contest} />
-        </td>
-        <td>
-          <span className={getRatingClass(event.rating)}>{event.rating.publicRating}</span>
-        </td>
-      </tr>
-    );
-  };
 }
