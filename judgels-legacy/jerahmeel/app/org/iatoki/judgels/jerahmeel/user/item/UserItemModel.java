53edfdd1196b69f0477d52467c56b635c4f2d746
Ashar Fuadi <fushar@gmail.com>
2019-10-07 00:00:27 +0700

    Fix Jerahmeel user_item index

--- judgels-legacy/jerahmeel/app/org/iatoki/judgels/jerahmeel/user/item/UserItemModel.java
+++ judgels-legacy/jerahmeel/app/org/iatoki/judgels/jerahmeel/user/item/UserItemModel.java
@@ -3,8 +3,11 @@ package org.iatoki.judgels.jerahmeel.user.item;
 import judgels.persistence.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 @Entity(name = "jerahmeel_user_item")
+@Table(indexes = {@Index(columnList = "userJid,itemJid", unique = true)})
 public final class UserItemModel extends Model {
     public String userJid;
 
