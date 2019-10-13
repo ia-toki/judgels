345a6fac5e99ecc4252773f645972734e1f162c7
Ashar Fuadi <fushar@gmail.com>
2019-10-13 11:03:13 +0700

    Remove removed class from Java 11

--- judgels-backends/judgels-commons/judgels-service-core/src/test/java/judgels/service/RandomCodeGeneratorTests.java
+++ judgels-backends/judgels-commons/judgels-service-core/src/test/java/judgels/service/RandomCodeGeneratorTests.java
@@ -14,10 +14,4 @@ class RandomCodeGeneratorTests {
     void generated_codes_are_unique() {
         assertThat(RandomCodeGenerator.newCode()).isNotEqualTo(RandomCodeGenerator.newCode());
     }
-
-    @Test
-    void generated_codes_are_lowercase() {
-        String code = RandomCodeGenerator.newCode();
-        assertThat(code).isEqualTo(code.toLowerCase());
-    }
 }
