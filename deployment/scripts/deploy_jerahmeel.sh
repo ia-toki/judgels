161be2bec7a0d68b9a5f1a9da5a13c3081b9d8c7
Ashar Fuadi <fushar@gmail.com>
2019-10-18 22:51:28 +0700

    Add Jerahmeel deployment to CI

--- deployment/scripts/deploy_jerahmeel.sh
+++ deployment/scripts/deploy_jerahmeel.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
-cd "$(dirname "$0")"/../../judgels-legacy/jerahmeel
+cd "$(dirname "$0")"/../../judgels-backends/jerahmeel/jerahmeel-dist
 
-../gradlew stagePlayBinaryDist
-rm -rf build/stage/playBinary/conf
-rm -rf build/stage/playBinary/lib/org.webjars-*
+../../gradlew clean distTar
+tar -xf build/distributions/jerahmeel-* --strip-components=1 -C build/distributions
+rm build/distributions/*.tgz
+cp init.sh build/distributions
 
-cd ../../deployment/ansible
+cd ../../../deployment/ansible
 
 ansible --version
 ansible-playbook -e @dist/env.yml -e judgels_version=$JUDGELS_VERSION playbooks/build-jerahmeel.yml
+ansible-playbook -e @dist/env.yml -e judgels_version=$JUDGELS_VERSION playbooks/deploy-jerahmeel.yml
