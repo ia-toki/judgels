647f3407046c2dea3597c668c2b8d2b1a43278b4
Ashar Fuadi <fushar@gmail.com>
2019-12-18 09:53:23 +0700

    Don't fail submissions table if container is not found

--- judgels-frontends/raphael/src/routes/submissions/SubmissionsTable/SubmissionsTable.tsx
+++ judgels-frontends/raphael/src/routes/submissions/SubmissionsTable/SubmissionsTable.tsx
@@ -115,6 +115,9 @@ export class SubmissionsTable extends React.PureComponent<SubmissionsTableProps>
   };
 
   private constructContainerUrl = (subpaths: string[]) => {
+    if (!subpaths) {
+      return '';
+    }
     if (subpaths.length === 2) {
       return `/courses/${subpaths[0]}/chapters/${subpaths[1]}/problems`;
     } else {
