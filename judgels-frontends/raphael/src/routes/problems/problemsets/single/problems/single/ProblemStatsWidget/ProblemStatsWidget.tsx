578b01f7b190a11bedfac245345d2178cfac0484
Ashar Fuadi <fushar@gmail.com>
2020-01-05 15:36:50 +0700

    Add top users stat in problem stat widget

--- judgels-frontends/raphael/src/routes/problems/problemsets/single/problems/single/ProblemStatsWidget/ProblemStatsWidget.tsx
+++ judgels-frontends/raphael/src/routes/problems/problemsets/single/problems/single/ProblemStatsWidget/ProblemStatsWidget.tsx
@@ -1,10 +1,17 @@
+import { HTMLTable } from '@blueprintjs/core';
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { connect } from 'react-redux';
 
 import { ContentCard } from '../../../../../../../components/ContentCard/ContentCard';
+import { UserRef } from '../../../../../../../components/UserRef/UserRef';
 import { ProblemSet } from '../../../../../../../modules/api/jerahmeel/problemSet';
-import { ProblemProgress, ProblemStats } from '../../../../../../../modules/api/jerahmeel/problem';
+import {
+  ProblemProgress,
+  ProblemStats,
+  ProblemTopStats,
+  ProblemTopStatsEntry,
+} from '../../../../../../../modules/api/jerahmeel/problem';
 import { ProblemStatsResponse } from '../../../../../../../modules/api/jerahmeel/problemSetProblem';
 import { selectProblemSet } from '../../../../modules/problemSetSelectors';
 import { problemSetProblemActions as injectedProblemSetProblemActions } from '../../modules/problemSetProblemActions';
@@ -22,7 +29,10 @@ interface ProblemStatsWidgetState {
 }
 
 class ProblemStatsWidget extends React.Component<ProblemStatsWidgetProps, ProblemStatsWidgetState> {
+  private static TOP_STATS_SIZE = 5;
+
   state: ProblemStatsWidgetState = {};
+
   async componentDidMount() {
     const response = await this.props.onGetProblemStats(
       this.props.problemSet.jid,
@@ -36,8 +46,13 @@ class ProblemStatsWidget extends React.Component<ProblemStatsWidgetProps, Proble
     if (!response) {
       return null;
     }
-    const { progress, stats } = response;
-    return <>{this.renderStats(progress, stats)}</>;
+    const { progress, stats, topStats, profilesMap } = response;
+    return (
+      <>
+        {this.renderStats(progress, stats)}
+        {this.renderTopStats(topStats, profilesMap)}
+      </>
+    );
   }
 
   private renderStats = (progress: ProblemProgress, stats: ProblemStats) => {
@@ -62,6 +77,74 @@ class ProblemStatsWidget extends React.Component<ProblemStatsWidgetProps, Proble
             Your score: <VerdictProgressTag {...progress} />
           </li>
         </ul>
+      </ContentCard>
+    );
+  };
+
+  private renderTopStats = (topStats: ProblemTopStats, profilesMap) => {
+    const { topUsersByScore, topUsersByTime, topUsersByMemory } = topStats;
+    if (
+      topUsersByScore.length === ProblemStatsWidget.TOP_STATS_SIZE &&
+      topUsersByScore[ProblemStatsWidget.TOP_STATS_SIZE - 1].stats >= 100
+    ) {
+      return (
+        <>
+          {this.renderTopTime(topUsersByTime, profilesMap)}
+          {this.renderTopMemory(topUsersByMemory, profilesMap)}
+        </>
+      );
+    } else {
+      return this.renderTopScore(topUsersByScore, profilesMap);
+    }
+  };
+
+  private renderTopScore = (entries: ProblemTopStatsEntry[], profilesMap) => {
+    return this.renderTopEntries(entries, profilesMap, 'score', 'Score', '');
+  };
+
+  private renderTopTime = (entries: ProblemTopStatsEntry[], profilesMap) => {
+    return this.renderTopEntries(entries, profilesMap, 'time', 'Time', 'ms');
+  };
+  private renderTopMemory = (entries: ProblemTopStatsEntry[], profilesMap) => {
+    return this.renderTopEntries(entries, profilesMap, 'memory', 'Memory', 'KB');
+  };
+
+  private renderTopEntries = (
+    entries: ProblemTopStatsEntry[],
+    profilesMap,
+    title: string,
+    header: string,
+    suffix: string
+  ) => {
+    if (entries.length === 0) {
+      return null;
+    }
+
+    const rows = entries.map((e, idx) => (
+      <tr key={e.userJid}>
+        <td>{idx + 1}</td>
+        <td>
+          <UserRef profile={profilesMap[e.userJid]} />
+        </td>
+        <td>
+          {e.stats} {suffix}
+        </td>
+      </tr>
+    ));
+
+    return (
+      <ContentCard className="problem-stats-widget">
+        <h4>Top users by {title}</h4>
+        <HTMLTable striped className="table-list stats-table">
+          <thead>
+            <tr>
+              <th className="col-rank">#</th>
+              <th>User</th>
+              <th>{header}</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </HTMLTable>
       </ContentCard>
     );
   };
