e55ec166aaeecfd76be88ad2ea93a1676aee4858
Ashar Fuadi <fushar@gmail.com>
2020-01-10 07:42:48 +0700

    Happy new year

--- judgels-frontends/raphael/src/components/Footer/Footer.tsx
+++ judgels-frontends/raphael/src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => (
   <div className="footer">
     <hr />
     <div className="footer__text">
-      <div className="footer__left">&copy; 2019 Ikatan Alumni TOKI</div>
+      <div className="footer__left">&copy; Ikatan Alumni TOKI</div>
       <div className="footer__right">
         Powered by <a href="https://github.com/ia-toki/judgels">Judgels</a>
       </div>
