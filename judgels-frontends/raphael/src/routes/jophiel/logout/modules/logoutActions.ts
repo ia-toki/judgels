dcb7aabbfc196b0cb8555b5ecb741960c1a099f3
Jonathan Christopher <jonathan.christopher@outlook.com>
2020-10-08 03:03:44 +0800

    Add new user-friendly error messages for login and logout UserMaxConcurrentSessionsExceeded and LogoutDisabled errors

--- judgels-frontends/raphael/src/routes/jophiel/logout/modules/logoutActions.ts
+++ judgels-frontends/raphael/src/routes/jophiel/logout/modules/logoutActions.ts
@@ -1,15 +1,21 @@
-import { UnauthorizedError } from '../../../../modules/api/error';
+import { UnauthorizedError, ForbiddenError } from '../../../../modules/api/error';
 import { JophielRole } from '../../../../modules/api/jophiel/role';
 import { DelSession } from '../../../../modules/session/sessionReducer';
 import { selectToken } from '../../../../modules/session/sessionSelectors';
 import { PutWebConfig } from '../../modules/userWebReducer';
 import { sessionAPI } from '../../../../modules/api/jophiel/session';
+import { SessionErrors } from '../../../../modules/api/jophiel/session';
 
 export function logOut(currentPath: string) {
   return async (dispatch, getState) => {
     try {
       await sessionAPI.logOut(selectToken(getState()));
     } catch (error) {
+      if (error instanceof ForbiddenError) {
+        if (error.message === SessionErrors.LogoutDisabled) {
+          throw new Error('Logout is currently disabled.')
+        }
+      }
       if (!(error instanceof UnauthorizedError)) {
         throw error;
       }
