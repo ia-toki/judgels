91623ce6a2ff59643a620d11af3de06b279905e9
Ashar Fuadi <fushar@gmail.com>
2020-12-08 21:11:12 +0700

    [refactor] Add more tests to contest routes

---
+++ judgels-frontends/raphael/src/routes/contests/contests/single/submissions/Programming/single/ContestSubmissionPage/ContestSubmissionPage.test.jsx
@@ -0,0 +1,67 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { MemoryRouter, Route } from 'react-router';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import ContestSubmissionPage from './ContestSubmissionPage';
+import { OutputOnlyOverrides } from '../../../../../../../../modules/api/gabriel/language';
+import webPrefsReducer, { PutStatementLanguage } from '../../../../../../../../modules/webPrefs/webPrefsReducer';
+import contestReducer, { PutContest } from '../../../../../modules/contestReducer';
+import * as contestSubmissionActions from '../../modules/contestSubmissionActions';
+
+jest.mock('../../modules/contestSubmissionActions');
+
+describe('ContestSubmissionPage', () => {
+  let wrapper;
+
+  beforeEach(async () => {
+    contestSubmissionActions.getSubmissionWithSource.mockReturnValue(() =>
+      Promise.resolve({
+        data: {
+          submission: {
+            id: 10,
+            gradingEngine: OutputOnlyOverrides.KEY,
+          },
+          source: {},
+        },
+      })
+    );
+
+    const store = createStore(
+      combineReducers({
+        webPrefs: webPrefsReducer,
+        uriel: combineReducers({ contest: contestReducer }),
+      }),
+      applyMiddleware(thunk)
+    );
+    store.dispatch(
+      PutContest({
+        jid: 'contestJid',
+      })
+    );
+    store.dispatch(PutStatementLanguage('en'));
+
+    wrapper = mount(
+      <IntlProvider locale={navigator.language}>
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/contest/contestSlug/submissions/submissions/10']}>
+            <Route
+              path="/contest/contestSlug/submissions/submissions/:submissionId"
+              component={ContestSubmissionPage}
+            />
+          </MemoryRouter>
+        </Provider>
+      </IntlProvider>
+    );
+
+    await new Promise(resolve => setImmediate(resolve));
+    wrapper.update();
+  });
+
+  test('page', () => {
+    expect(wrapper.text()).toContain('Submission #10');
+  });
+});
