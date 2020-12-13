a174cc320b4594e7b952d724b29823ae7e54fb3c
lungsin <kweelungsin@gmail.com>
2020-12-13 16:52:07 +0800

    Fix message body notification

--- judgels-frontends/raphael/src/modules/notification/notification.js
+++ judgels-frontends/raphael/src/modules/notification/notification.js
@@ -1,7 +1,8 @@
 import logo from '../../assets/images/logo-header.png';
+import { APP_CONFIG } from '../../conf';
 
 export function showDesktopNotification(title, tag, message) {
-  new Notification(title, { body: `${APP_CONFIG.name}: message`, icon: logo, tag: tag });
+  new Notification(title, { body: `${APP_CONFIG.name}: ${message}`, icon: logo, tag: tag });
 }
 
 export function askDesktopNotificationPermission() {
