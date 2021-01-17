519d9297049f3ba513561ffcdca671ef610235e6
Ashar Fuadi <fushar@gmail.com>
2021-01-17 10:19:17 +0700

    Remove unused JudgelsModule in Sandalphon

--- judgels-backends/sandalphon/sandalphon-app/app/org/iatoki/judgels/play/JudgelsModule.java
+++
@@ -1,9 +0,0 @@
-package org.iatoki.judgels.play;
-
-import com.google.inject.AbstractModule;
-
-public final class JudgelsModule extends AbstractModule {
-
-    @Override
-    protected void configure() {}
-}
