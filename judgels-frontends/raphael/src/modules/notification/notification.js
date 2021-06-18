1bf778dcd3ef2884c203a92db6a5e9959c55df99
Ashar Fuadi <fushar@gmail.com>
2021-06-18 22:28:04 +0700

    Only show desktop notification if Notification api is present on the browser

--- judgels-frontends/raphael/src/modules/notification/notification.js
+++ judgels-frontends/raphael/src/modules/notification/notification.js
@@ -2,9 +2,15 @@ import logo from '../../assets/images/logo-header.png';
 import { APP_CONFIG } from '../../conf';
 
 export function showDesktopNotification(title, tag, message) {
+  if (!('Notification' in window)) {
+    return;
+  }
   new Notification(title, { body: `${APP_CONFIG.name}: ${message}`, icon: logo, tag: tag });
 }
 
 export function askDesktopNotificationPermission() {
+  if (!('Notification' in window)) {
+    return;
+  }
   Notification.requestPermission(result => {});
 }
