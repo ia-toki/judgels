2564fd4ad651bce4b75fe864c3d562d522a39261
Ashar Fuadi <fushar@gmail.com>
2022-01-30 10:28:04 +0700

    Change http method of upsert users endpoint from PUT to POST

--- judgels-backends/jophiel/jophiel-api/src/main/java/judgels/jophiel/api/user/UserService.java
+++ judgels-backends/jophiel/jophiel-api/src/main/java/judgels/jophiel/api/user/UserService.java
@@ -11,7 +11,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -38,7 +37,7 @@ public interface UserService {
     @Produces(TEXT_PLAIN)
     String exportUsers(@HeaderParam(AUTHORIZATION) AuthHeader authHeader, List<String> usernames);
 
-    @PUT
+    @POST
     @Path("/batch-upsert")
     @Consumes(TEXT_PLAIN)
     @Produces(APPLICATION_JSON)
