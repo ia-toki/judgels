53c93c3b0bbba60b6bcfdc2575947b6c03726503
Ashar Fuadi <fushar@gmail.com>
2022-11-08 09:22:52 -0700

    UI improvements: simplify Submissions page

--- judgels-frontends/raphael/src/routes/submissions/SubmissionsRoutes.jsx
+++ judgels-frontends/raphael/src/routes/submissions/SubmissionsRoutes.jsx
@@ -1,6 +1,5 @@
 import { Route, withRouter, Switch } from 'react-router';
 
-import { Card } from '../../components/Card/Card';
 import { FullPageLayout } from '../../components/FullPageLayout/FullPageLayout';
 import SubmissionsPage from './SubmissionsPage/SubmissionsPage';
 import SubmissionPage from './single/SubmissionPage/SubmissionPage';
@@ -9,14 +8,12 @@ import { withBreadcrumb } from '../../components/BreadcrumbWrapper/BreadcrumbWra
 function SubmissionsRoutes() {
   return (
     <FullPageLayout>
-      <Card className="submissions-routes" title="Submissions">
-        <Switch>
-          <Route exact path="/submissions" component={SubmissionsPage} />
-          <Route exact path="/submissions/mine" component={SubmissionsPage} />
+      <Switch>
+        <Route exact path="/submissions" component={SubmissionsPage} />
+        <Route exact path="/submissions/mine" component={SubmissionsPage} />
 
-          <Route path="/submissions/:submissionId" component={SubmissionPage} />
-        </Switch>
-      </Card>
+        <Route path="/submissions/:submissionId" component={SubmissionPage} />
+      </Switch>
     </FullPageLayout>
   );
 }
