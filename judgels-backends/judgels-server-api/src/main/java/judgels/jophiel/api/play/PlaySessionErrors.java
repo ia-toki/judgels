a24707f1dcb8f99a6c3602a4b70dbd88bddcb6dd
Ashar Fuadi <fushar@gmail.com>
2023-05-08 21:27:06 +0700

    refactor(v2): remove unused play code

--- judgels-backends/judgels-server-api/src/main/java/judgels/jophiel/api/play/PlaySessionErrors.java
+++
@@ -1,7 +0,0 @@
-package judgels.jophiel.api.play;
-
-public class PlaySessionErrors {
-    private PlaySessionErrors() {}
-
-    public static final String ROLE_NOT_ALLOWED = "Jophiel:RoleNotAllowed";
-}
