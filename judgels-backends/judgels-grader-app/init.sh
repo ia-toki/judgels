61737345c7b3757f15124a50ed05caadd19ab284
Ashar Fuadi <fushar@gmail.com>
2023-05-18 20:23:28 +0700

    feat: simplify Dockerfile command logic especially for db migrate
    [force ci]

--- judgels-backends/judgels-grader-app/init.sh
+++ judgels-backends/judgels-grader-app/init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 set -ex
-
-exec ./bin/judgels-grader-app server var/conf/judgels-grader.yml
+exec ./bin/judgels-grader-app $@ var/conf/judgels-grader.yml
--- judgels-backends/judgels-server-app/init.sh
+++ judgels-backends/judgels-server-app/init.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 set -ex
-
-if [[ "$1" == "dbMigrate" ]]; then
-    exec ./bin/judgels-server-app db migrate var/conf/judgels-server.yml
-else
-    exec ./bin/judgels-server-app server var/conf/judgels-server.yml
-fi
+exec ./bin/judgels-server-app $@ var/conf/judgels-server.yml
