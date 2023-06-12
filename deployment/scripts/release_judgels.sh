9d8b70848ea02c70554a6ef3401890a9ac54d3ff
Ashar Fuadi <fushar@gmail.com>
2023-06-12 22:35:42 +0700

    fix(ansible): fix wrong env variable when releasing judgels

--- deployment/scripts/release_judgels.sh
+++ deployment/scripts/release_judgels.sh
@@ -5,4 +5,4 @@ set -ex
 cd deployment/ansible
 
 ansible --version
-ansible-playbook -e app_version=$app_version playbooks/release-judgels.yml
+ansible-playbook -e app_version=$APP_VERSION playbooks/release-judgels.yml
