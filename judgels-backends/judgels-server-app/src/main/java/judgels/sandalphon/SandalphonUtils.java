bde6e75334f4bc603b4572d6395b791ef8f1c7c2
Ashar Fuadi <fushar@gmail.com>
2023-06-15 07:13:23 +0700

    hack(sandalphon): hardcode render urls with https if not served from localhost

--- judgels-backends/judgels-server-app/src/main/java/judgels/sandalphon/SandalphonUtils.java
+++ judgels-backends/judgels-server-app/src/main/java/judgels/sandalphon/SandalphonUtils.java
@@ -24,41 +24,62 @@ public class SandalphonUtils {
     }
 
     public static String replaceProblemRenderUrls(String text, String baseUrl, String problemJid) {
+        // HACKHACK
+        // TODO(fushar): get the scheme from X-Forwarded-Proto
+        String apiUrl = baseUrl;
+        if (!apiUrl.startsWith("http://localhost") && !apiUrl.startsWith("http://127.0.0.1")) {
+            apiUrl = apiUrl.replace("http://", "https://");
+        }
+
         return text
                 .replaceAll(
                         "src=\"render/",
-                        String.format("src=\"%sapi/v2/problems/%s/render/", baseUrl, problemJid))
+                        String.format("src=\"%sapi/v2/problems/%s/render/", apiUrl, problemJid))
                 .replaceAll(
                         "url=render/",
-                        String.format("url=%sapi/v2/problems/%s/render/", baseUrl, problemJid))
+                        String.format("url=%sapi/v2/problems/%s/render/", apiUrl, problemJid))
                 .replaceAll(
                         "href=\"render/",
-                        String.format("href=\"%sapi/v2/problems/%s/render/", baseUrl, problemJid));
+                        String.format("href=\"%sapi/v2/problems/%s/render/", apiUrl, problemJid));
     }
 
     public static String replaceProblemEditorialRenderUrls(String text, String baseUrl, String problemJid) {
+        // HACKHACK
+        // TODO(fushar): get the scheme from X-Forwarded-Proto
+        String apiUrl = baseUrl;
+        if (!apiUrl.startsWith("http://localhost") && !apiUrl.startsWith("http://127.0.0.1")) {
+            apiUrl = apiUrl.replace("http://", "https://");
+        }
+
         return text
                 .replaceAll(
                         "src=\"render/",
-                        String.format("src=\"%sapi/v2/problems/%s/editorials/render/", baseUrl, problemJid))
+                        String.format("src=\"%sapi/v2/problems/%s/editorials/render/", apiUrl, problemJid))
                 .replaceAll(
                         "url=render/",
-                        String.format("url=%sapi/v2/problems/%s/editorials/render/", baseUrl, problemJid))
+                        String.format("url=%sapi/v2/problems/%s/editorials/render/", apiUrl, problemJid))
                 .replaceAll(
                         "href=\"render/",
-                        String.format("href=\"%sapi/v2/problems/%s/editorials/render/", baseUrl, problemJid));
+                        String.format("href=\"%sapi/v2/problems/%s/editorials/render/", apiUrl, problemJid));
     }
 
     public static String replaceLessonRenderUrls(String text, String baseUrl, String lessonJid) {
+        // HACKHACK
+        // TODO(fushar): get the scheme from X-Forwarded-Proto
+        String apiUrl = baseUrl;
+        if (!apiUrl.startsWith("http://localhost") && !apiUrl.startsWith("http://127.0.0.1")) {
+            apiUrl = apiUrl.replace("http://", "https://");
+        }
+
         return text
                 .replaceAll(
                         "src=\"render/",
-                        String.format("src=\"%sapi/v2/lessons/%s/render/", baseUrl, lessonJid))
+                        String.format("src=\"%sapi/v2/lessons/%s/render/", apiUrl, lessonJid))
                 .replaceAll(
                         "url=render/",
-                        String.format("url=%sapi/v2/lessons/%s/render/", baseUrl, lessonJid))
+                        String.format("url=%sapi/v2/lessons/%s/render/", apiUrl, lessonJid))
                 .replaceAll(
                         "href=\"render/",
-                        String.format("href=\"%sapi/v2/lessons/%s/render/", baseUrl, lessonJid));
+                        String.format("href=\"%sapi/v2/lessons/%s/render/", apiUrl, lessonJid));
     }
 }
