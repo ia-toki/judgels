66b8b18f9b27eed9e1cdf1853153158576c9958b
Ashar Fuadi <fushar@gmail.com>
2023-09-16 19:56:34 +0700

    fix(client): update chapter problem icon for bundle problems

--- judgels-client/src/routes/courses/courses/single/chapters/single/resources/ChapterResourcesPage/ChapterResourcesPage.test.jsx
+++ judgels-client/src/routes/courses/courses/single/chapters/single/resources/ChapterResourcesPage/ChapterResourcesPage.test.jsx
@@ -117,8 +117,8 @@ describe('ChapterResourcesPage', () => {
       expect(cards.map(card => [card.text(), card.find('a').props().href])).toEqual([
         ['bookX. Lesson X', '/courses/courseSlug/chapters/chapter-1/lessons/X'],
         ['bookY. Lesson Y', '/courses/courseSlug/chapters/chapter-1/lessons/Y'],
-        ['panel-tableA. Problem A🎉 solved  confirm', '/courses/courseSlug/chapters/chapter-1/problems/A'],
-        ['panel-tableB. Problem B', '/courses/courseSlug/chapters/chapter-1/problems/B'],
+        ['formA. Problem A🎉 solved  confirm', '/courses/courseSlug/chapters/chapter-1/problems/A'],
+        ['formB. Problem B', '/courses/courseSlug/chapters/chapter-1/problems/B'],
       ]);
     });
   });
