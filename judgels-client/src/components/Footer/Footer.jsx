26f6b715ba1750221af51cd41c48d57fdcef5cf1
Ashar Fuadi <fushar@gmail.com>
2023-10-06 20:16:00 +0700

    feat(client): support footer (#521)

--- judgels-client/src/components/Footer/Footer.jsx
+++ judgels-client/src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
+import HTMLReactParser from 'html-react-parser';
+
+import { APP_CONFIG } from '../../conf';
+
 import './Footer.scss';
 
 export function Footer() {
+  const footer = APP_CONFIG.footer || '© Ikatan Alumni TOKI';
+
   return (
     <div className="footer">
       <hr />
       <small className="footer__text">
-        <div className="float-left">&copy; Ikatan Alumni TOKI</div>
+        <div className="float-left">{HTMLReactParser(footer)}</div>
         <div className="float-right">
           Powered by <a href="https://github.com/ia-toki/judgels">Judgels</a>
         </div>
