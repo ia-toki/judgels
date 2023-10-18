c8c320943fc61a21f1a2f5a98fc5b5d067db6ef5
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2023-10-18 20:25:02 +0800

    chore(problem): change default memory limit to 256 MB (#536)

--- judgels-backends/judgels-grader-engines/src/main/java/judgels/gabriel/engines/DefaultConfig.java
+++ judgels-backends/judgels-grader-engines/src/main/java/judgels/gabriel/engines/DefaultConfig.java
@@ -2,7 +2,7 @@ package judgels.gabriel.engines;
 
 public class DefaultConfig {
     public static final int TIME_LIMIT = 2000;
-    public static final int MEMORY_LIMIT = 65536;
+    public static final int MEMORY_LIMIT = 262144;
 
     private DefaultConfig() {}
 }
