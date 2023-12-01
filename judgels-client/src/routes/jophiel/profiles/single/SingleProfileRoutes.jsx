b5e8eae35d8f2b576715f6f41d95ab8da7850883
Reynaldo  Wijaya Hendry <wijayareynaldo@gmail.com>
2023-12-01 23:35:19 +0800

    fix: Back not reflect proper state in profile (#559)
    Co-authored-by: Reynaldo WIJAYA HENDRY <reynaldo_wijaya_hendry@tech.gov.sg>

--- judgels-client/src/routes/jophiel/profiles/single/SingleProfileRoutes.jsx
+++ judgels-client/src/routes/jophiel/profiles/single/SingleProfileRoutes.jsx
@@ -12,8 +12,10 @@ import ContestHistoryPage from './contestHistory/ContestHistoryPage/ContestHisto
 import SubmissionHistoryPage from './submissionHistory/SubmissionHistoryPage/SubmissionHistoryPage';
 import { selectUserJid, selectUsername } from '../../modules/profileSelectors';
 
-function SingleProfileRoutes({ userJid, username }) {
-  if (!userJid) {
+function SingleProfileRoutes({ match, userJid, username }) {
+  // Optimization:
+  // We wait until we get the username from the backend only if the current username is different from the persisted one.
+  if (!userJid || username !== match.params.username) {
     return <LoadingState large />;
   }
 
