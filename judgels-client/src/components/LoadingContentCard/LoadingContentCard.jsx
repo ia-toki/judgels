8aded7d7c81cc34322d8285d8b10a8978c6c82e1
Ashar Fuadi <fushar@gmail.com>
2024-01-06 12:48:41 +0700

    feat(client): Add timer to LoadingContentCard

--- judgels-client/src/components/LoadingContentCard/LoadingContentCard.jsx
+++ judgels-client/src/components/LoadingContentCard/LoadingContentCard.jsx
@@ -1,14 +1,40 @@
 import { Classes } from '@blueprintjs/core';
+import { Component } from 'react';
 
 import { ContentCard } from '../ContentCard/ContentCard';
 
-export function LoadingContentCard() {
-  return (
-    <ContentCard>
-      <h4 className={Classes.SKELETON}>This is a placeholder for a long content name</h4>
-      <p className={Classes.SKELETON}>
-        <small>Placeholder for content description</small>
-      </p>
-    </ContentCard>
-  );
+export class LoadingContentCard extends Component {
+  timer;
+
+  state = {
+    showSkeleton: false,
+  };
+
+  componentDidMount() {
+    this.timer = setTimeout(() => {
+      this.setState({ showSkeleton: true });
+      this.timer = 0;
+    }, 200);
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = 0;
+    }
+  }
+
+  render() {
+    if (!this.state.showSkeleton) {
+      return null;
+    }
+    return (
+      <ContentCard>
+        <h4 className={Classes.SKELETON}>This is a placeholder for a long content name</h4>
+        <p className={Classes.SKELETON}>
+          <small>Placeholder for content description</small>
+        </p>
+      </ContentCard>
+    );
+  }
 }
