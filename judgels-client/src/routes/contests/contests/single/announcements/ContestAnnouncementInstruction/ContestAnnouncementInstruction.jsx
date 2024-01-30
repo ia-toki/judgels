0333a1e11f73a0514ee5d250b8e54d5515e683ec
Izhari Ishak Aksa <izharishaksa@gmail.com>
2024-01-30 19:18:41 +0700

    remove unused css class

--- judgels-client/src/routes/contests/contests/single/announcements/ContestAnnouncementInstruction/ContestAnnouncementInstruction.jsx
+++ judgels-client/src/routes/contests/contests/single/announcements/ContestAnnouncementInstruction/ContestAnnouncementInstruction.jsx
@@ -2,7 +2,7 @@ import { Callout } from "@blueprintjs/core";
 
 export function ContestAnnouncementInstruction() {
   return (
-    <Callout className="bp5-icon-info-sign heading-with-button-action">
+    <Callout className="bp5-icon-info-sign">
       <h5>Add Image to Your Contest Announcement</h5>
       <ol>
         <li>
