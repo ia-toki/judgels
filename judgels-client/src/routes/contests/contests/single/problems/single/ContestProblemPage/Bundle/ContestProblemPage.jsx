29faa16e1e8cf1431795e8b3f1326e9a8ab7b547
Ashar Fuadi <fushar@gmail.com>
2024-02-11 21:48:02 +0700

    fix(client): redirect to results correctly on contest bundle problem submission

--- judgels-client/src/routes/contests/contests/single/problems/single/ContestProblemPage/Bundle/ContestProblemPage.jsx
+++ judgels-client/src/routes/contests/contests/single/problems/single/ContestProblemPage/Bundle/ContestProblemPage.jsx
@@ -91,12 +91,15 @@ export class ContestProblemPage extends Component {
     if (!latestSubmissions) {
       return <LoadingState />;
     }
+
+    const resultsUrl = `/contests/${this.props.contest.slug}/submissions`;
     return (
       <ProblemWorksheetCard
         alias={problem.alias}
         latestSubmissions={latestSubmissions}
         onAnswerItem={this.onCreateSubmission}
         worksheet={worksheet}
+        resultsUrl={resultsUrl}
       />
     );
   };
