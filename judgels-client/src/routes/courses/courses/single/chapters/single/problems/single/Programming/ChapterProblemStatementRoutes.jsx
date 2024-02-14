0c5470b23442dfbf16934065f8787ca0aa55bb1b
Ashar Fuadi <fushar@gmail.com>
2024-02-14 16:27:33 +0700

    feat(client): rename Submit tab to Code (#600)

--- judgels-client/src/routes/courses/courses/single/chapters/single/problems/single/Programming/ChapterProblemStatementRoutes.jsx
+++ judgels-client/src/routes/courses/courses/single/chapters/single/problems/single/Programming/ChapterProblemStatementRoutes.jsx
@@ -10,7 +10,7 @@ export default function ChapterProblemStatementRoutes({ worksheet, renderNavigat
   const topbarItems = [
     {
       id: '@',
-      title: 'Submit',
+      title: 'Code',
       routeComponent: Route,
       render: props => (
         <ChapterProblemWorkspacePage {...props} worksheet={worksheet} renderNavigation={renderNavigation} />
