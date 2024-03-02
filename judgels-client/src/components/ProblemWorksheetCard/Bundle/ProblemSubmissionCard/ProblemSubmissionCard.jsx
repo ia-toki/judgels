85c8b9ebfb3b9b3cd0c24797d77061ccf6fb41b2
Ashar Fuadi <fushar@gmail.com>
2024-03-02 14:27:21 +0700

    feat(client): disable bundle submission when there are unconfirmed answers (#609)

--- judgels-client/src/components/ProblemWorksheetCard/Bundle/ProblemSubmissionCard/ProblemSubmissionCard.jsx
+++ judgels-client/src/components/ProblemWorksheetCard/Bundle/ProblemSubmissionCard/ProblemSubmissionCard.jsx
@@ -5,6 +5,13 @@ import { ContentCard } from '../../../ContentCard/ContentCard';
 
 export function ProblemSubmissionCard({ reasonNotAllowedToSubmit, resultsUrl }) {
   const redirectToResultsUrl = () => {
+    const unconfirmedShortAnswers = document.querySelectorAll('input[type="text"]:not([readonly])');
+    const unconfirmedEssays = document.querySelectorAll('textarea:not([readonly])');
+    if (unconfirmedShortAnswers.length > 0 || unconfirmedEssays.length > 0) {
+      window.alert('There are still unconfirmed answers! Make sure to click "Confirm answer" button.');
+      return;
+    }
+
     window.location.href = resultsUrl;
   };
 
