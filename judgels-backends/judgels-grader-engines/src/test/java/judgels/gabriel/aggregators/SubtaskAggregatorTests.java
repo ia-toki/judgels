c4f851bc8d0b89667c46cd9da73180834e8e1277
Ashar Fuadi <fushar@gmail.com>
2024-08-26 12:24:19 +0700

    grader: show SKIPPED for subtasks without submitted files in OO problems (#658)

--- judgels-backends/judgels-grader-engines/src/test/java/judgels/gabriel/aggregators/SubtaskAggregatorTests.java
+++ judgels-backends/judgels-grader-engines/src/test/java/judgels/gabriel/aggregators/SubtaskAggregatorTests.java
@@ -2,7 +2,6 @@ package judgels.gabriel.aggregators;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.common.collect.ImmutableList;
 import java.util.List;
 import judgels.gabriel.api.SubtaskVerdict;
 import judgels.gabriel.api.Verdict;
@@ -19,12 +18,22 @@ class SubtaskAggregatorTests {
 
     @Test
     void aggregate() {
-        List<SubtaskVerdict> subtaskVerdicts = ImmutableList.of(
+        List<SubtaskVerdict> subtaskVerdicts = List.of(
                 new SubtaskVerdict.Builder().verdict(Verdict.ACCEPTED).points(10).build(),
                 new SubtaskVerdict.Builder().verdict(Verdict.TIME_LIMIT_EXCEEDED).points(20).build(),
                 new SubtaskVerdict.Builder().verdict(Verdict.OK).points(30).build(),
+                new SubtaskVerdict.Builder().verdict(Verdict.SKIPPED).points(0).build(),
                 new SubtaskVerdict.Builder().verdict(Verdict.WRONG_ANSWER).points(0).build());
 
         assertThat(aggregator.aggregate(subtaskVerdicts)).isEqualTo(SubtaskVerdict.of(Verdict.TIME_LIMIT_EXCEEDED, 60));
+    }
+
+    @Test
+    void aggregate_skipped() {
+        List<SubtaskVerdict> subtaskVerdicts = List.of(
+                new SubtaskVerdict.Builder().verdict(Verdict.SKIPPED).points(0).build(),
+                new SubtaskVerdict.Builder().verdict(Verdict.SKIPPED).points(0).build());
+
+        assertThat(aggregator.aggregate(subtaskVerdicts)).isEqualTo(SubtaskVerdict.of(Verdict.OK, 0));
     }
 }
