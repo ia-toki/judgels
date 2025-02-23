127ab68a4a8496a673eba0713f3b0b3960457228
Ashar Fuadi <fushar@gmail.com>
2025-02-23 14:00:25 +0700

    chore: Upgrade Java 11 -> 17

--- judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/ContestScoreboard.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/ContestScoreboard.java
@@ -2,14 +2,12 @@ package judgels.uriel.api.contest.scoreboard;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.NoClass;
 import java.time.Instant;
 import judgels.uriel.api.contest.ContestStyle;
 import org.immutables.value.Value;
 
 @Value.Immutable
-@JsonDeserialize(as = ImmutableContestScoreboard.class)
 public interface ContestScoreboard {
     ContestScoreboardType getType();
     ContestStyle getStyle();
