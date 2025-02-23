d158b52ba1df5d8e947c2168bf185cee2af614d6
Ashar Fuadi <fushar@gmail.com>
2025-02-23 14:28:55 +0700

    Prepare for release 3.0.0
    [force ci]

--- deployment/scripts/build_judgels_client.sh
+++ deployment/scripts/build_judgels_client.sh
@@ -12,4 +12,4 @@ rm -rf dist/build && mv build dist/
 cd ../deployment/ansible
 
 ansible --version 0</dev/null |& cat -
-ansible-playbook playbooks/build-judgels-client.yml 0</dev/null |& cat -
+ansible-playbook  -e app_version=3.0.0-alpha playbooks/build-judgels-client.yml 0</dev/null |& cat -
