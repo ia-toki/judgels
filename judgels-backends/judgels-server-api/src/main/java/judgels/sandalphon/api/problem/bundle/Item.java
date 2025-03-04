21b8072735e0630970ce93d4cade51d30e726e83
Ashar Fuadi <fushar@gmail.com>
2025-03-04 22:48:52 +0700

    Upgrade Java 11 -> 17

--- judgels-backends/judgels-server-api/src/main/java/judgels/sandalphon/api/problem/bundle/Item.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/sandalphon/api/problem/bundle/Item.java
@@ -1,7 +1,5 @@
 package judgels.sandalphon.api.problem.bundle;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.util.Optional;
 import org.immutables.value.Value;
@@ -13,19 +11,6 @@ public interface Item {
     ItemType getType();
     Optional<Integer> getNumber();
     String getMeta();
-
-    @JsonTypeInfo(
-            use = JsonTypeInfo.Id.NAME,
-            include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
-            property = "type",
-            visible = true
-    )
-    @JsonSubTypes({
-            @JsonSubTypes.Type(value = ImmutableStatementItemConfig.class),
-            @JsonSubTypes.Type(value = ImmutableMultipleChoiceItemConfig.class),
-            @JsonSubTypes.Type(value = ImmutableShortAnswerItemConfig.class),
-            @JsonSubTypes.Type(value = ImmutableEssayItemConfig.class)
-    })
     ItemConfig getConfig();
 
     class Builder extends ImmutableItem.Builder {}
--- judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/ContestScoreboard.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/ContestScoreboard.java
@@ -1,9 +1,6 @@
 package judgels.uriel.api.contest.scoreboard;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.NoClass;
 import java.time.Instant;
 import judgels.uriel.api.contest.ContestStyle;
 import org.immutables.value.Value;
@@ -15,21 +12,6 @@ public interface ContestScoreboard {
     ContestStyle getStyle();
     int getTotalEntries();
     Instant getUpdatedTime();
-
-    @JsonTypeInfo(
-            use = JsonTypeInfo.Id.NAME,
-            include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
-            property = "style",
-            visible = true,
-            defaultImpl = NoClass.class
-    )
-    @JsonSubTypes({
-            @JsonSubTypes.Type(value = ImmutableBundleScoreboard.class),
-            @JsonSubTypes.Type(value = ImmutableGcjScoreboard.class),
-            @JsonSubTypes.Type(value = ImmutableIcpcScoreboard.class),
-            @JsonSubTypes.Type(value = ImmutableIoiScoreboard.class),
-            @JsonSubTypes.Type(value = ImmutableTrocScoreboard.class)
-    })
     Scoreboard getScoreboard();
 
     class Builder extends ImmutableContestScoreboard.Builder {}
