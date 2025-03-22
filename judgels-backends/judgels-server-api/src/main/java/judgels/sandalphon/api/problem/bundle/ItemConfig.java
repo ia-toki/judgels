db98bfec08e736a5a0dfb0bb1645c77a7e52e2d3
Ashar Fuadi <fushar@gmail.com>
2025-03-22 15:39:48 +0700

    Upgrade Java 11 -> 17 (#681)

--- judgels-backends/judgels-server-api/src/main/java/judgels/sandalphon/api/problem/bundle/ItemConfig.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/sandalphon/api/problem/bundle/ItemConfig.java
@@ -1,5 +1,15 @@
 package judgels.sandalphon.api.problem.bundle;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ImmutableStatementItemConfig.class),
+        @JsonSubTypes.Type(value = ImmutableMultipleChoiceItemConfig.class),
+        @JsonSubTypes.Type(value = ImmutableShortAnswerItemConfig.class),
+        @JsonSubTypes.Type(value = ImmutableEssayItemConfig.class)
+})
 public interface ItemConfig {
     String getStatement();
 }
