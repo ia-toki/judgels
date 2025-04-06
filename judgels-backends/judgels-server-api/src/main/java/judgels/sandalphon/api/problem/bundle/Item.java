a580cefeba73234fd83a0f62690369cfa2125b30
Ashar Fuadi <fushar@gmail.com>
2025-04-06 21:38:30 +0700

    Java 17: fix @JsonSubTypes usage

--- judgels-backends/judgels-server-api/src/main/java/judgels/sandalphon/api/problem/bundle/Item.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/sandalphon/api/problem/bundle/Item.java
@@ -1,6 +1,9 @@
 package judgels.sandalphon.api.problem.bundle;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.NoClass;
 import java.util.Optional;
 import org.immutables.value.Value;
 
@@ -11,6 +14,20 @@ public interface Item {
     ItemType getType();
     Optional<Integer> getNumber();
     String getMeta();
+
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+            property = "type",
+            visible = true,
+            defaultImpl = NoClass.class
+    )
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = StatementItemConfig.class),
+            @JsonSubTypes.Type(value = MultipleChoiceItemConfig.class),
+            @JsonSubTypes.Type(value = ShortAnswerItemConfig.class),
+            @JsonSubTypes.Type(value = EssayItemConfig.class)
+    })
     ItemConfig getConfig();
 
     class Builder extends ImmutableItem.Builder {}
--- judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/Scoreboard.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/Scoreboard.java
@@ -1,16 +1,5 @@
 package judgels.uriel.api.contest.scoreboard;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "style")
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = ImmutableBundleScoreboard.class),
-        @JsonSubTypes.Type(value = ImmutableGcjScoreboard.class),
-        @JsonSubTypes.Type(value = ImmutableIcpcScoreboard.class),
-        @JsonSubTypes.Type(value = ImmutableIoiScoreboard.class),
-        @JsonSubTypes.Type(value = ImmutableTrocScoreboard.class)
-})
 public interface Scoreboard {
     ScoreboardState getState();
     ScoreboardContent getContent();
