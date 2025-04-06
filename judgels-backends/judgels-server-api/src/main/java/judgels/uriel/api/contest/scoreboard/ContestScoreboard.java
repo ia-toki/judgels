532c376f49d173d0b8e44f907983b22cf7581b28
Ashar Fuadi <fushar@gmail.com>
2025-04-06 21:47:03 +0700

    Java 17: fix @JsonSubTypes usage (#700)

--- judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/ContestScoreboard.java
+++ judgels-backends/judgels-server-api/src/main/java/judgels/uriel/api/contest/scoreboard/ContestScoreboard.java
@@ -1,6 +1,9 @@
 package judgels.uriel.api.contest.scoreboard;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.NoClass;
 import java.time.Instant;
 import judgels.uriel.api.contest.ContestStyle;
 import org.immutables.value.Value;
@@ -12,6 +15,21 @@ public interface ContestScoreboard {
     ContestStyle getStyle();
     int getTotalEntries();
     Instant getUpdatedTime();
+
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+            property = "style",
+            visible = true,
+            defaultImpl = NoClass.class
+    )
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = BundleScoreboard.class),
+            @JsonSubTypes.Type(value = GcjScoreboard.class),
+            @JsonSubTypes.Type(value = IcpcScoreboard.class),
+            @JsonSubTypes.Type(value = IoiScoreboard.class),
+            @JsonSubTypes.Type(value = TrocScoreboard.class)
+    })
     Scoreboard getScoreboard();
 
     class Builder extends ImmutableContestScoreboard.Builder {}
