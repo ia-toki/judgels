72a0f830f8bd8c9415d62dc38ef1ca780d73f06a
Ashar Fuadi <fushar@gmail.com>
2025-08-16 17:13:20 +0700

    Allow textarea submission in problem set problem as well (#757)

--- judgels-client/src/routes/problems/problemsets/single/problems/single/submissions/modules/problemSetSubmissionActions.js
+++ judgels-client/src/routes/problems/problemsets/single/problems/single/submissions/modules/problemSetSubmissionActions.js
@@ -1,5 +1,6 @@
 import { push } from 'connected-react-router';
 
+import { getGradingLanguageEditorSubmissionFilename } from '../../../../../../../../modules/api/gabriel/language';
 import { submissionProgrammingAPI } from '../../../../../../../../modules/api/jerahmeel/submissionProgramming';
 import { selectToken } from '../../../../../../../../modules/session/sessionSelectors';
 import { selectIsDarkMode } from '../../../../../../../../modules/webPrefs/webPrefsSelectors';
@@ -23,18 +24,19 @@ export function getSubmissionWithSource(submissionId, language) {
 export function createSubmission(problemSetSlug, problemSetJid, problemAlias, problemJid, data) {
   return async (dispatch, getState) => {
     const token = selectToken(getState());
-    let sourceFiles = {};
-    Object.keys(data.sourceFiles).forEach(key => {
-      sourceFiles['sourceFiles.' + key] = data.sourceFiles[key];
+    let sources = {};
+    Object.keys(data.sourceTexts ?? []).forEach(key => {
+      sources['sourceFiles.' + key] = new File(
+        [data.sourceTexts[key]],
+        getGradingLanguageEditorSubmissionFilename(data.gradingLanguage),
+        { type: 'text/plain' }
+      );
+    });
+    Object.keys(data.sourceFiles ?? []).forEach(key => {
+      sources['sourceFiles.' + key] = data.sourceFiles[key];
     });
 
-    await submissionProgrammingAPI.createSubmission(
-      token,
-      problemSetJid,
-      problemJid,
-      data.gradingLanguage,
-      sourceFiles
-    );
+    await submissionProgrammingAPI.createSubmission(token, problemSetJid, problemJid, data.gradingLanguage, sources);
 
     toastActions.showSuccessToast('Solution submitted.');
 
