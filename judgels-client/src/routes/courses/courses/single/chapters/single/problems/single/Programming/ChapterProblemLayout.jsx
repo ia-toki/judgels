d421743cbf8ff11c911d66b5ffaace65b473db54
Ashar Fuadi <fushar@gmail.com>
2026-02-10 22:55:03 +0700

    Fix broken code workspace in courses after refactor

--- judgels-client/src/routes/courses/courses/single/chapters/single/problems/single/Programming/ChapterProblemLayout.jsx
+++ judgels-client/src/routes/courses/courses/single/chapters/single/problems/single/Programming/ChapterProblemLayout.jsx
@@ -1,10 +1,6 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { Outlet, useParams } from '@tanstack/react-router';
-import { useSelector } from 'react-redux';
 
 import ContentWithTopbar from '../../../../../../../../../components/ContentWithTopbar/ContentWithTopbar';
-import { courseBySlugQueryOptions, courseChapterQueryOptions } from '../../../../../../../../../modules/queries/course';
-import { selectToken } from '../../../../../../../../../modules/session/sessionSelectors';
 import { ChapterProblemContext } from '../ChapterProblemContext';
 import ChapterProblemStatementPage from './ChapterProblemStatementPage/ChapterProblemStatementPage';
 
@@ -13,9 +9,6 @@ import './ChapterProblemStatementLayout.scss';
 
 export default function ChapterProblemLayout({ worksheet, renderNavigation }) {
   const { courseSlug, chapterAlias } = useParams({ strict: false });
-  const token = useSelector(selectToken);
-  const { data: course } = useSuspenseQuery(courseBySlugQueryOptions(token, courseSlug));
-  const { data: chapter } = useSuspenseQuery(courseChapterQueryOptions(token, course.jid, chapterAlias));
   const problemAlias = worksheet?.problem?.alias;
 
   const topbarItems = [
@@ -29,7 +22,7 @@ export default function ChapterProblemLayout({ worksheet, renderNavigation }) {
     },
   ];
 
-  const basePath = `/courses/${courseSlug}/chapters/${chapter?.alias}/problems/${problemAlias}`;
+  const basePath = `/courses/${courseSlug}/chapters/${chapterAlias}/problems/${problemAlias}`;
 
   return (
     <div className="chapter-programming-problem-page">
