f9c75f1ab5a11860569d0ae3c2ef4bb45d695158
Ashar Fuadi <fushar@gmail.com>
2026-02-28 08:39:51 +0700

    Refactor: submissions (#852)

--- judgels-client/src/modules/queries/submissionProgramming.js
+++ judgels-client/src/modules/queries/submissionProgramming.js
@@ -1,7 +1,16 @@
 import { queryOptions } from '@tanstack/react-query';
 
 import { submissionProgrammingAPI } from '../api/jerahmeel/submissionProgramming';
+import { queryClient } from '../queryClient';
 import { getToken } from '../session';
+
+export const submissionsQueryOptions = params => {
+  const { username, page } = params || {};
+  return queryOptions({
+    queryKey: ['submissions', ...(params ? [params] : [])],
+    queryFn: () => submissionProgrammingAPI.getSubmissions(getToken(), undefined, username, undefined, undefined, page),
+  });
+};
 
 export const submissionWithSourceQueryOptions = (submissionId, params) => {
   const { language } = params || {};
@@ -9,4 +18,19 @@ export const submissionWithSourceQueryOptions = (submissionId, params) => {
     queryKey: ['submissions', submissionId, 'source', ...(params ? [params] : [])],
     queryFn: () => submissionProgrammingAPI.getSubmissionWithSource(getToken(), submissionId, language),
   });
+};
+
+export const regradeSubmissionMutationOptions = {
+  mutationFn: submissionJid => submissionProgrammingAPI.regradeSubmission(getToken(), submissionJid),
+  onSuccess: () => {
+    queryClient.invalidateQueries(submissionsQueryOptions());
+  },
+};
+
+export const regradeSubmissionsMutationOptions = {
+  mutationFn: ({ username } = {}) =>
+    submissionProgrammingAPI.regradeSubmissions(getToken(), undefined, username, undefined, undefined),
+  onSuccess: () => {
+    queryClient.invalidateQueries(submissionsQueryOptions());
+  },
 };
