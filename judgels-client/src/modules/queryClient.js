b9df6ec97cfff3b8927f068fc66a265664eb202c
Ashar Fuadi <fushar@gmail.com>
2026-03-02 04:59:08 +0700

    Add prefetchQuery() to submissions + add staleTime

--- judgels-client/src/modules/queryClient.js
+++ judgels-client/src/modules/queryClient.js
@@ -7,6 +7,7 @@ import * as toastActions from './toast/toastActions';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      staleTime: 5 * 1000, // 5 seconds
       gcTime: 1000 * 60 * 60 * 24, // 24 hours
     },
     mutations: {
