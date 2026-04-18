2ca296242346d6bf7b675be55fe8f465f0108e47
Ashar Fuadi <fushar@gmail.com>
2026-04-18 09:05:43 +0900

    Block pelatnas usernames

--- judgels-client/src/routes/blockedUsernames.js
+++ judgels-client/src/routes/blockedUsernames.js
@@ -1,22 +1,22 @@
 // Temporary list of usernames blocked from accessing submissions, courses, and problems
 // during selection tests. Remove this file when the restriction is no longer needed.
-export const BLOCKED_USERNAMES = [];
-//   'warren030310',
-//   'mitchell.jh',
-//   'thesen',
-//   'Retr0Foxx',
-//   'LC24',
-//   'Andra_MHT_16',
-//   'kalifpermadi',
-//   'hitsuuj',
-//   'marchell_hii',
-//   'sakka',
-//   'RKHTM',
-//   'evanrusly',
-//   'Jesslyn_nathania19',
-//   'Bananade',
-//   'yusuf12360',
-// ];
+export const BLOCKED_USERNAMES = [
+  'warren030310',
+  'mitchell.jh',
+  'thesen',
+  'Retr0Foxx',
+  'LC24',
+  'Andra_MHT_16',
+  'kalifpermadi',
+  'hitsuuj',
+  'marchell_hii',
+  'sakka',
+  'RKHTM',
+  'evanrusly',
+  'Jesslyn_nathania19',
+  'Bananade',
+  'yusuf12360',
+];
 
 export function isUserBlocked(user) {
   return BLOCKED_USERNAMES.includes(user?.username);
