51aa89da058da9b7a8a42dc4e4e6197eef3b5f4c
VioAlbert <vioalbert08@gmail.com>
2026-08-17 10:08:45 +0700

    Fix header filter results banner text wrapping in ProblemSetsPage (#981)

--- judgels-client/src/routes/problems/problemsets/ProblemSetsPage/ProblemSetsPage.test.jsx
+++ judgels-client/src/routes/problems/problemsets/ProblemSetsPage/ProblemSetsPage.test.jsx
@@ -12,13 +12,13 @@ describe('ProblemSetsPage', () => {
     setSession('token', { jid: 'userJid' });
   });
 
-  const renderComponent = async response => {
+  const renderComponent = async (response, initialEntries = ['/']) => {
     nockApi().get('/problemsets').query(true).reply(200, response);
 
     await act(async () =>
       render(
         <QueryClientProviderWrapper>
-          <TestRouter>
+          <TestRouter initialEntries={initialEntries}>
             <ProblemSetsPage />
           </TestRouter>
         </QueryClientProviderWrapper>
@@ -39,6 +39,46 @@ describe('ProblemSetsPage', () => {
 
     expect(await screen.findByText('Problemset One')).toBeInTheDocument();
     expect(screen.getByText(/most recently added problemsets/i)).toBeInTheDocument();
+  });
+
+  test('renders archive filter banner', async () => {
+    await renderComponent(
+      {
+        data: {
+          page: [{ jid: 'JIDPS1', slug: 'ps-1', name: 'Problemset One', archiveJid: 'JIDARC1', description: '' }],
+          totalCount: 1,
+        },
+        archiveName: 'Archive One',
+        archiveDescriptionsMap: {},
+        problemSetProgressesMap: {},
+        profilesMap: {},
+      },
+      ['/problemsets?archive=archive-1']
+    );
+
+    expect(await screen.findByText('Problemset One')).toBeInTheDocument();
+    expect(screen.getByText(/problemsets in archive/i)).toBeInTheDocument();
+    expect(screen.getByText('Archive One')).toBeInTheDocument();
+  });
+
+  test('renders search results banner', async () => {
+    await renderComponent(
+      {
+        data: {
+          page: [{ jid: 'JIDPS1', slug: 'ps-1', name: 'Problemset One', archiveJid: 'JIDARC1', description: '' }],
+          totalCount: 1,
+        },
+        archiveName: 'Archive One',
+        archiveDescriptionsMap: {},
+        problemSetProgressesMap: {},
+        profilesMap: {},
+      },
+      ['/problemsets?name=foo&archive=archive-1']
+    );
+
+    expect(await screen.findByText('Problemset One')).toBeInTheDocument();
+    expect(screen.getByText(/search results for:/i)).toBeInTheDocument();
+    expect(screen.getByText('foo')).toBeInTheDocument();
   });
 
   test('renders empty placeholder', async () => {
